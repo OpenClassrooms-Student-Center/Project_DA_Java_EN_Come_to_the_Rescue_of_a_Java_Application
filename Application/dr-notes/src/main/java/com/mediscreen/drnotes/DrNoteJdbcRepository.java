31a7b7fc62350afde1ec52662dfeb67f8c0ba378
nathalie <nathalie@localhost>
2019-11-20 17:37:12 +0100

    Removed unnecessary files Adding omissions Spelling correction

--- Application/dr-notes/src/main/java/com/mediscreen/drnotes/DrNoteJdbcRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mediscreen.drnotes;
-
-import java.util.List;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DrNoteJdbcRepository extends MongoRepository<DrNote, String> {
-	public List<DrNote> findByPatientId(Long patientId);
-}
