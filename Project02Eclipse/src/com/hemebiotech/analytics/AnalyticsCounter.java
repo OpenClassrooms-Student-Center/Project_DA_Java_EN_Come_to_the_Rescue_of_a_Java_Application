334ebfed971ccf6261f52af87829dbd2f31598b7
MrgnCpn <morgan310977@gmail.com>
2020-02-14 16:18:16 +0100

    Logic implementation in AnalyticsCounter.Main()

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,41 +3,35 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Enumeration;
+import java.util.Hashtable;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
+		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
+		FileWriter writer = new FileWriter("result.out");
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+
+		String line = reader.readLine();
+		Hashtable<String, Integer> symptoms = new Hashtable<String, Integer>();
+		
+		while (line != null) {
+			if (symptoms.containsKey(line)) {
+				symptoms.put(line, symptoms.get(line) + 1);
+			} else {
+				symptoms.put(line, 1);
+			}
+			line = reader.readLine();
+		}
+
+		Enumeration<String> symptomsKeys = symptoms.keys();
+		
+		while(symptomsKeys.hasMoreElements()) {
+	    	String key = symptomsKeys.nextElement();
+	    	writer.write(key + " : " + symptoms.get(key) + "\n");
+	    }
+		
+		reader.close();
 		writer.close();
 	}
 }
