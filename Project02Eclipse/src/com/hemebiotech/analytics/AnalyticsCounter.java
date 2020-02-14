1a2443d9974a795970c47997f7300fc01ad210f0
MrgnCpn <morgan310977@gmail.com>
2020-02-14 16:23:37 +0100

    Exception Management

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,35 +3,38 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
 public class AnalyticsCounter {
 	public static void main(String args[]) throws Exception {
-		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
-		FileWriter writer = new FileWriter("result.out");
-		
-
-		String line = reader.readLine();
-		Hashtable<String, Integer> symptoms = new Hashtable<String, Integer>();
-		
-		while (line != null) {
-			if (symptoms.containsKey(line)) {
-				symptoms.put(line, symptoms.get(line) + 1);
-			} else {
-				symptoms.put(line, 1);
+		try (
+				BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
+				FileWriter writer = new FileWriter("result.out")
+			) {
+			
+	
+			String line = reader.readLine();
+			Hashtable<String, Integer> symptoms = new Hashtable<String, Integer>();
+			
+			while (line != null) {
+				if (symptoms.containsKey(line)) {
+					symptoms.put(line, symptoms.get(line) + 1);
+				} else {
+					symptoms.put(line, 1);
+				}
+				line = reader.readLine();
 			}
-			line = reader.readLine();
+	
+			Enumeration<String> symptomsKeys = symptoms.keys();
+			
+			while(symptomsKeys.hasMoreElements()) {
+		    	String key = symptomsKeys.nextElement();
+		    	writer.write(key + " : " + symptoms.get(key) + "\n");
+		    }
+		} catch (IOException e) {
+			  e.printStackTrace();
 		}
-
-		Enumeration<String> symptomsKeys = symptoms.keys();
-		
-		while(symptomsKeys.hasMoreElements()) {
-	    	String key = symptomsKeys.nextElement();
-	    	writer.write(key + " : " + symptoms.get(key) + "\n");
-	    }
-		
-		reader.close();
-		writer.close();
 	}
 }
