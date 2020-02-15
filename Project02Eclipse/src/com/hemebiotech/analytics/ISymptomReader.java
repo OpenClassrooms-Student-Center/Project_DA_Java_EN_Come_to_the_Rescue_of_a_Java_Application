1363772b92f0d556e92964cb2cd30c8e44cc7592
MrgnCpn <morgan310977@gmail.com>
2020-02-15 19:14:40 +0100

    Sorting Data from Map options (Sort by key / Sort by value)

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -4,4 +4,6 @@ import java.util.Map;
 
 public interface ISymptomReader {
 	Map<String, Integer> GetSymptoms();
+	Map<String, Integer> sortSymptomsByKey(Map<String, Integer> symptomsList);
+	Map<String, Integer> sortSymptomsByValue(Map<String, Integer> symptomsList);
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomsDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomsDataFromFile.java
@@ -3,6 +3,12 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.TreeMap;
 import java.util.Map;
 
@@ -14,7 +20,7 @@ public class ReadSymptomsDataFromFile implements ISymptomReader {
 	}
 	
 	public Map<String, Integer> GetSymptoms() {
-		Map<String, Integer> symptoms = new TreeMap<String, Integer>();
+		Map<String, Integer> symptoms = new HashMap<String, Integer>();
 		
 		if (this.filePath != null || this.filePath != "") {
 			try ( BufferedReader reader = new BufferedReader (new FileReader(this.filePath))) {
@@ -37,5 +43,27 @@ public class ReadSymptomsDataFromFile implements ISymptomReader {
 		}
 		
 		return symptoms;
+	}
+	
+	public Map<String, Integer> sortSymptomsByKey(Map<String, Integer> symptomsList) {
+		Map<String, Integer> sortedSymptomsByKey = new TreeMap<String, Integer>(symptomsList);
+		return sortedSymptomsByKey;
+	}
+	
+	public Map<String, Integer> sortSymptomsByValue(Map<String, Integer> symptomsList) {
+        List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(symptomsList.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        Map<String, Integer> sortedSymptomsByValue = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> entry : list) {
+        	sortedSymptomsByValue.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedSymptomsByValue;
 	}
 }
