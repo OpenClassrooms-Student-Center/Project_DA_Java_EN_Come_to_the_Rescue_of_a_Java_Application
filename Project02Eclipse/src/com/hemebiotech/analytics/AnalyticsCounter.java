0dfd841712172a4688b0dfdcfdfef5b82ad75576
MrgnCpn <morgan310977@gmail.com>
2020-02-15 19:59:02 +0100

    Sort by Key to Alphabetical output

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -11,13 +11,13 @@ public class AnalyticsCounter {
 	 * Program who read list of symptoms in file and write then with their count in output file
 	 * @param args
 	 * @throws Exception
-	 */ 
+	 */
 	public static void main(String args[]) throws Exception {
 		ReadSymptomsDataFromFile symptomsFileReader = new ReadSymptomsDataFromFile("Project02Eclipse/symptoms.txt");
 		WriteSymptomsDataToFile symptomsFileWriter = new WriteSymptomsDataToFile("Project02Eclipse/result.out");
 		
 		symptomsFileWriter.writeSymptoms(
-				symptomsFileReader.sortSymptomsByValue(
+				symptomsFileReader.sortSymptomsByKey(
 						symptomsFileReader.GetSymptoms()
 				),
 		true, true);
