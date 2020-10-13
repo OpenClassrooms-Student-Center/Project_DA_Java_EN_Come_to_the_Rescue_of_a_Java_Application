2de0af8e429a22f9dc845efe46808a404bc12175
adrbessy <adrienbessy@hotmail.com>
2020-10-13 15:26:44 +0200

    Exceptions handling; closing resources; variables don't overlap anymore; suppression of useless comments; the file symptoms.txt can be read; the number of occurrences of headache, rash and pupils are specified in result.out; newLine function instead of anti-slash n.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,78 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
+
+	public static void main(String[] args) {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
+		BufferedReader reader = null;
+		File file = new File("symptoms.txt");
+		try {
+			reader = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			System.err.printf("The file %s was not found.", file.toString());
+			System.exit(-1);
 		}
-		
+
+		String line = null;
+		try {
+			line = reader.readLine();
+			while (line != null) {
+				System.out.println("symptom from file: " + line);
+				if (line.equals("headache")) {
+					headacheCount++;
+					System.out.println("number of headaches: " + headacheCount);
+				} else if (line.equals("rash")) {
+					rashCount++;
+				} else if (line.contains("pupils")) {
+					pupilCount++;
+				}
+				line = reader.readLine(); // get another symptom
+			}
+		} catch (IOException e) {
+			System.err.printf("The file %s cannot be read.", file.toString());
+			System.exit(-1);
+		}
+		try {
+			reader.close();
+		} catch (IOException e1) {
+			System.err.printf("The file %s cannot be closed.", file.toString());
+			System.exit(-1);
+		} /*
+			 * catch (NullPointerException e) {
+			 * System.err.printf("Impossible to open the file %s.", file.toString());
+			 * System.exit(-1); }
+			 */
+
 		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		BufferedWriter writer = null;
+		File file2 = new File("result.out");
+		try {
+			writer = new BufferedWriter(new FileWriter(file2));
+			writer.write("headache: " + headacheCount);
+			writer.newLine();
+			writer.write("rash: " + rashCount);
+			writer.newLine();
+			writer.write("pupils: " + pupilCount);
+		} catch (IOException e) {
+			System.err.printf("Impossible to write in the file %s.", file2.toString());
+			System.exit(-1);
+		}
+		try {
+			writer.close();
+		} catch (IOException e) {
+			System.err.printf("The file %s cannot be closed.", file2.toString());
+			System.exit(-1);
+		}
 	}
 }
