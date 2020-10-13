268832355d26b3e9d489d6f46b9922777501104e
adrbessy <adrienbessy@hotmail.com>
2020-10-13 18:45:51 +0200

    New functionality : count the occurrences of any symptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -7,11 +7,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
 
 	public static void main(String[] args) {
 		// first get input
@@ -24,18 +23,24 @@ public class AnalyticsCounter {
 			System.exit(-1);
 		}
 
+		// count occurrences of each symptom
+		Map<String, Integer> symptomsCounts = new TreeMap<String, Integer>();
+
 		String line = null;
 		try {
 			line = reader.readLine();
 			while (line != null) {
-				System.out.println("symptom from file: " + line);
-				if (line.equals("headache")) {
-					headacheCount++;
-					System.out.println("number of headaches: " + headacheCount);
-				} else if (line.equals("rash")) {
-					rashCount++;
-				} else if (line.contains("pupils")) {
-					pupilCount++;
+				System.out.println("Processing line: " + line);
+
+				if (line == null || line.trim().equals("")) {
+					continue;
+				}
+				String processed = line.toLowerCase();
+
+				if (symptomsCounts.containsKey(processed)) {
+					symptomsCounts.put(processed, symptomsCounts.get(processed) + 1);
+				} else {
+					symptomsCounts.put(processed, 1);
 				}
 				line = reader.readLine(); // get another symptom
 			}
@@ -48,22 +53,20 @@ public class AnalyticsCounter {
 		} catch (IOException e1) {
 			System.err.printf("The file %s cannot be closed.", file.toString());
 			System.exit(-1);
-		} /*
-			 * catch (NullPointerException e) {
-			 * System.err.printf("Impossible to open the file %s.", file.toString());
-			 * System.exit(-1); }
-			 */
+		}
 
 		// next generate output
 		BufferedWriter writer = null;
-		File file2 = new File("result.out");
+		File file2 = new File("symptom_occurrence.out");
 		try {
 			writer = new BufferedWriter(new FileWriter(file2));
-			writer.write("headache: " + headacheCount);
-			writer.newLine();
-			writer.write("rash: " + rashCount);
-			writer.newLine();
-			writer.write("pupils: " + pupilCount);
+
+			for (String symptom : symptomsCounts.keySet()) {
+				int count = symptomsCounts.get(symptom);
+				writer.write(symptom + " : " + count);
+				writer.newLine();
+			}
+
 		} catch (IOException e) {
 			System.err.printf("Impossible to write in the file %s.", file2.toString());
 			System.exit(-1);
