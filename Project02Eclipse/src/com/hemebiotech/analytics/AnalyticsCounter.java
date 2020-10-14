f7e08967252189237e264319dd51b5ae71c665a0
adrbessy <adrienbessy@hotmail.com>
2020-10-14 18:31:04 +0200

    New class SymptomCounter. Added JavaDoc.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,59 +1,25 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * Date: Oct 14-2020. This is a simple application for counting occurrences of
+ * symptoms listed in a file.
+ * 
+ * @author adrien
+ * @version 2.0
+ *
+ */
 public class AnalyticsCounter {
 
 	public static void main(String[] args) {
-		// first get input
-		BufferedReader reader = null;
-		File file = new File("symptoms.txt");
-		try {
-			reader = new BufferedReader(new FileReader(file));
-		} catch (FileNotFoundException e) {
-			System.err.printf("The file %s was not found.", file.toString());
-			System.exit(-1);
-		}
 
-		// count occurrences of each symptom
-		Map<String, Integer> symptomsCounts = new TreeMap<String, Integer>();
-
-		String line = null;
-		try {
-			line = reader.readLine();
-			while (line != null) {
-				System.out.println("Processing line: " + line);
-
-				if (line == null || line.trim().equals("")) {
-					continue;
-				}
-				String processed = line.toLowerCase();
-
-				if (symptomsCounts.containsKey(processed)) {
-					symptomsCounts.put(processed, symptomsCounts.get(processed) + 1);
-				} else {
-					symptomsCounts.put(processed, 1);
-				}
-				line = reader.readLine(); // get another symptom
-			}
-		} catch (IOException e) {
-			System.err.printf("The file %s cannot be read.", file.toString());
-			System.exit(-1);
-		}
-		try {
-			reader.close();
-		} catch (IOException e1) {
-			System.err.printf("The file %s cannot be closed.", file.toString());
-			System.exit(-1);
-		}
+		SymptomCounter symptomCounter = new SymptomCounter("symptoms.txt");
+		TreeMap<String, Integer> symptomsCounts = symptomCounter.getSymptomOccurrences();
 
 		// next generate output
 		BufferedWriter writer = null;
