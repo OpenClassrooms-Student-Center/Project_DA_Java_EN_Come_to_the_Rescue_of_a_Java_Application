f73749e06d60d4677134cec8aaddd1a60d143cf6
adrbessy <adrienbessy@hotmail.com>
2020-10-15 13:25:21 +0200

    Use of the class ReadSymptomDataFromFile and creation of the class WriteSymptomOccurrenceToFile. More JavaDoc comments and generation of javadoc html.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,9 +1,6 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.List;
 import java.util.TreeMap;
 
 /**
@@ -18,30 +15,14 @@ public class AnalyticsCounter {
 
 	public static void main(String[] args) {
 
-		SymptomCounter symptomCounter = new SymptomCounter("symptoms.txt");
-		TreeMap<String, Integer> symptomsCounts = symptomCounter.getSymptomOccurrences();
+		ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile("symptoms.txt");
+		List<String> symptomsList = readSymptomDataFromFile.GetSymptoms();
 
-		// next generate output
-		BufferedWriter writer = null;
-		File file2 = new File("symptom_occurrence.out");
-		try {
-			writer = new BufferedWriter(new FileWriter(file2));
+		SymptomCounter symptomCounter = new SymptomCounter(symptomsList);
+		TreeMap<String, Integer> symptomsCounts = symptomCounter.GetSymptomOccurrences();
 
-			for (String symptom : symptomsCounts.keySet()) {
-				int count = symptomsCounts.get(symptom);
-				writer.write(symptom + " : " + count);
-				writer.newLine();
-			}
-
-		} catch (IOException e) {
-			System.err.printf("Impossible to write in the file %s.", file2.toString());
-			System.exit(-1);
-		}
-		try {
-			writer.close();
-		} catch (IOException e) {
-			System.err.printf("The file %s cannot be closed.", file2.toString());
-			System.exit(-1);
-		}
+		WriteSymptomOccurrenceToFile writeSymptomOccurrenceToFile = new WriteSymptomOccurrenceToFile(
+				"symptom_occurrence.out", symptomsCounts);
+		writeSymptomOccurrenceToFile.writeOccurences();
 	}
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,47 +1,74 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Simple brute force implementation
- *
+ * Date: Oct 15-2020. This class read the symptoms in a file.
+ * 
+ * @author adrien
+ * @version 2.0
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
+	private List<String> symptomsList = null;
+
 	/**
 	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 * @param filepath a full or partial path to file with symptom strings in it,
+	 *                 one per line
 	 */
-	public ReadSymptomDataFromFile (String filepath) {
+	public ReadSymptomDataFromFile(String filepath) {
 		this.filepath = filepath;
+		symptomsList = new ArrayList<String>();
 	}
-	
-	@Override
+
+	/**
+	 * Read symptoms in a file and write them in a list
+	 * 
+	 * @return a List of String of symptoms
+	 */
 	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
-				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
-				}
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		BufferedReader reader = null;
+		File file = new File(filepath);
+		try {
+			reader = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			System.err.printf("The file %s was not found.", file.toString());
+			System.exit(-1);
 		}
-		
-		return result;
+		String line = null;
+		try {
+			line = reader.readLine();
+			while (line != null) {
+				System.out.println("Processing line: " + line);
+
+				if (line == null || line.trim().equals("")) {
+					continue;
+				}
+				String processed = line.toLowerCase();
+
+				symptomsList.add(processed);
+
+				line = reader.readLine(); // get another symptom
+			}
+		} catch (IOException e) {
+			System.err.printf("The file %s cannot be read.", file.toString());
+			System.exit(-1);
+		}
+		try {
+			reader.close();
+		} catch (IOException e1) {
+			System.err.printf("The file %s cannot be closed.", file.toString());
+			System.exit(-1);
+		}
+		return symptomsList;
 	}
 
 }
