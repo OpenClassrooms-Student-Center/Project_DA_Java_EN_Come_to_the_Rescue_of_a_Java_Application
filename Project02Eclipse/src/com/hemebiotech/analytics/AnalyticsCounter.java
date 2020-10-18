d3e13898c4d5a220c5e400a6b4965978273de674
Fouad-GitDev <fouad.djellouli@hotmail.fr>
2020-10-18 13:46:01 +0200

    Handling of all symptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,57 @@
 package com.hemebiotech.analytics;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
+
+/**
+ * Classe permettant de compter le nombre de symptomes dans un fichier texte
+ * @author Fouad
+ * 
+ */
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	
+/**
+ * 	
+ * @param args
+ * @throws Exception
+ */
 	
 	public static void main(String args[]) throws Exception {
+
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		
+		 ArrayList <String> list_symptoms = new ArrayList<String>();
 		while (line != null) {
-			i++;	// increment i
+			
 			System.out.println("symptom from file: " + line);
-			if (line.contains("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.contains("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+			list_symptoms.add(line);
 
 			line = reader.readLine();	// get another symptom
+			
 		}
+		
+		reader.close();
+		
+		HashMap<String, Integer> symptoms_map = new HashMap<>();
+		for(int j=0; j<list_symptoms.size(); j++) {
+		symptoms_map.put(list_symptoms.get(j), 1);	
+			
+		}
+		
 		
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		for (Entry<String, Integer> entry : symptoms_map.entrySet()) {
+			writer.write(entry.getKey()+":" + entry.getValue() + "\n");	
+		}
+		
+		
 		writer.close();
 	}
 }
