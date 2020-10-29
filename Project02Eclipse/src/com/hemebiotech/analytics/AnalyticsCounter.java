cf2a015b33ce4255535197ee2d6c75a6922ae39a
Sabouni Youssef <ysabouni@hsk-partners.com>
2020-10-29 16:04:47 +0100

    refactor(AnalyticCounter): Removing unused variables

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -11,14 +11,12 @@ public class AnalyticsCounter {
     private static int pupilCount    = 0;        // initialize to 0
 
     public static void main(String[] args) throws Exception {
+
         // first get input
         BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
         String         line   = reader.readLine();
 
-        int i         = 0;    // set i to 0
-        int headCount = 0;    // counts headaches
         while (line != null) {
-			i++;    // increment i
             System.out.println("symptom from file: " + line);
             if (line.equals("headache")) {
                 headacheCount++;
