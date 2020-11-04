afdf55c074a581fb2a70b4aa27874a3f3a21f251
veroniquebohe <veroniquebohe@iphonedeestelle.home>
2020-11-04 14:40:38 +0100

    modif code exception

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,8 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
+
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
+	public static int headCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
 	
@@ -13,31 +14,44 @@ public class AnalyticsCounter {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
+		
+		
+		
 		int headCount = 0;	// counts headaches
+		
 		while (line != null) {
-			i++;	// increment i
+			
 			System.out.println("symptom from file: " + line);
+			
+			 
+			
 			if (line.equals("headache")) {
 				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				System.out.println("number of headache: " + headCount);
+				
 			}
-			else if (line.equals("rush")) {
+			
+			else if (line.equals("rash")) {
 				rashCount++;
+				System.out.println("number of rashs: "  + rashCount);
 			}
-			else if (line.contains("pupils")) {
+			else if (line.equals("dialated pupils")) {
 				pupilCount++;
+				System.out.println("number of dialated pupils: "  + pupilCount);
 			}
-
-			line = reader.readLine();	// get another symptom
+			 
+			line = reader.readLine();// get another symptom
+			 
+			
 		}
 		
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
+		
+		writer.write("dialated pupils: " + pupilCount + "\n");		
+		writer.write("headache: " + headCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
+		reader.close();
 	}
 }
