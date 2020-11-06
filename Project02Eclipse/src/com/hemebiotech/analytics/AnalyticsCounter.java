69f8328a8ca3ac167b62fae9f79d375ae878cde5
johanna <tristanjohanna@gmail.com>
2020-11-06 10:44:38 +0100

    premier commit

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -8,31 +8,31 @@ public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
-	
+
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		BufferedReader reader = new BufferedReader(new FileReader("/home/johanna/Bureau/workspace_OCR/P2/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt"));
 		String line = reader.readLine();
 
 		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		int headachesCount = 0;	// counts headaches
 		while (line != null) {
 			i++;	// increment i
-			System.out.println("symptom from file: " + line);
+//			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				headacheCount++;
+				System.out.println("number of headaches: " + headacheCount);
 			}
-			else if (line.equals("rush")) {
+			else if (line.equals("rash")) {
 				rashCount++;
 			}
-			else if (line.contains("pupils")) {
+			else if (line.equals("dialated pupils")) {
 				pupilCount++;
 			}
 
 			line = reader.readLine();	// get another symptom
 		}
-		
+
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
