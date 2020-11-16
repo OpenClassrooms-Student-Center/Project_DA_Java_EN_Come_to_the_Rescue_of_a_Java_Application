11854fa8229b7a53a0aa869a02eaf83bc07ee650
veroniquebohe <veroniquebohe@ipad-des-filles.home>
2020-11-16 19:00:32 +0100

    AnalyticsCounter modifié pour lire les symptomes et nombres d'occurences

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,10 +3,126 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;// Package contenant la classe de gestion des exceptions
+import java.util.ArrayList;// Package contenant la classe ArrayList
+import java.util.List;// Package contenant la classe List
+import java.util.Map;// Package contenant la classe Map (collection)
+import java.util.TreeMap;// Package contenant la classe TreeMap (triée par ordre alphabétique)
 
 
-public class AnalyticsCounter {
-	public static int headCount = 0;	// initialize to 0
+
+
+public class AnalyticsCounter {  
+
+	public static void main(String args[]) throws Exception { // first get input
+	
+	//dossier contenant les symptomes
+	String docFile = "symptoms.txt";
+	
+	//liste des symptomes
+    List<String>symptoms = new ArrayList<String>();
+	
+	
+		
+		if (docFile != null) {
+			
+			try {
+				BufferedReader reader = new BufferedReader (new FileReader(docFile));
+				
+				String line = reader.readLine();
+				
+				while (line != null) {
+					
+					symptoms.add(line);
+					
+					line = reader.readLine();
+				}
+				reader.close();
+				
+				//permet de récupérer une éventuelle erreur d'exception. La méthode
+				// "printStackTrace" permet d'afficher les lignes du code en erreur
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		//carte des symptomes
+		Map<String,Integer>mapSym=new TreeMap<String,Integer>();
+			
+			for(String s :symptoms) {
+				
+			if(!mapSym.containsKey(s)) {
+			
+			mapSym.put(s,1);
+			
+			}else {
+			 mapSym.put(s,mapSym.get(s)+1);
+			
+			 }
+			
+			}
+			
+		for(String key : mapSym.keySet()) {
+			
+		System.out.println( key +"="+ mapSym.get(key));
+		
+		}
+			
+    
+
+
+
+
+       // next generate output
+       FileWriter writer = new FileWriter ("result.out");
+    
+        writer.write(String.valueOf(mapSym));
+       
+        writer.close();
+       
+
+}
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	/*public static int headCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
 	
@@ -54,4 +170,4 @@ public class AnalyticsCounter {
 		writer.close();
 		reader.close();
 	}
-}
+}*/
