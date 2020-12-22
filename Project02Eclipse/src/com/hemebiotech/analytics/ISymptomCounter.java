ee183e4a5fce66e370946e50d5338ad116c7707d
Fouzia09 <hfouzia@hotmail.fr>
2020-12-22 22:46:48 +0100

    correction projet

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomCounter.java
@@ -0,0 +1,11 @@
+package com.hemebiotech.analytics;
+
+
+import java.util.List;
+import java.util.Map;
+
+public interface ISymptomCounter {
+
+	
+	Map<String, Integer> countSymptoms(List<String> list) ;
+}
