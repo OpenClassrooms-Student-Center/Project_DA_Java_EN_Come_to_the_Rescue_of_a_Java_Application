8ca2b519b5268375368c2349785e4b09560758c4
andjamba <aurelien.ndjamba@gmail.com>
2021-01-04 14:38:58 +0100

    implémentations de la méthode abstraite définie dans ISymptomWriter

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomRateFromFile.java
@@ -0,0 +1,58 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Class ExportSymptomsRate: representing a file export in a "result.out file"
+ */
+public class WriteSymptomRateFromFile implements ISymptomWriter {
+	
+	/**
+	 * 
+	 * Méthode générant un dictionnaire - HashMap clé(maux) et valeur(occurrence)
+	 * 
+	 * @param SymptomsRate: 
+	 * @author aurelien.ndjamba
+	 * @version 1.0
+	 * 
+	 */
+	@Override
+	public void writerSymptomsToFile(Map<String, Integer> SymptomsRate) {
+
+		try {
+
+			FileWriter writer = new FileWriter("result.out");
+			
+			// Obtenir l'ensemble des clés_valeurs sous forme de set
+			Set<Map.Entry<String,Integer>> set = SymptomsRate.entrySet();
+			
+			// Obtenir l'iterator pour parcourir le set
+	        Iterator<Map.Entry<String,Integer>> it = set.iterator();
+			
+			while (it.hasNext()) {
+				Map.Entry<String,Integer> mentry = it.next();
+				writer.write(mentry.getKey() + " : "
+						+ mentry.getValue() + "\n");
+			}
+			
+			writer.close();
+			
+		} 
+		catch (FileNotFoundException e) {
+			System.out.println("the named file does not exist");
+
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+		finally {
+//			writer.close();
+		}
+	}
+	
+}
