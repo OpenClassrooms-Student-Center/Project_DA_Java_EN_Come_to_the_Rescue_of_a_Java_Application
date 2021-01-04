be4d20c2b76028262fe6c38e9851808485b61e71
JCabrol <justine.cabrol14@gmail.com>
2021-01-04 12:30:35 +0100

    Part reading data
    Remove code reading symptoms from the main to ReadSymptomDataFromFile.
    Changes in iSymptomReader : use a Treemap instead of a list to avoid
    duplications and to order symptoms in alphabetical order.
    Change getSymptoms method in ReadSymptomDataFromFile to implement
    correctly the new interface.

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,10 +1,11 @@
 package com.hemebiotech.analytics;
 
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.TreeMap;
+
 
 /**
  * Simple brute force implementation
@@ -12,36 +13,51 @@ import java.util.List;
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
+
 	private String filepath;
-	
+
+
 	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 *
+	 * @param filepath a full or partial path to file with symptom strings in it, one per line.
 	 */
 	public ReadSymptomDataFromFile (String filepath) {
 		this.filepath = filepath;
 	}
-	
+
+
 	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
+
+
+	public TreeMap <String, Integer> getSymptoms() {
+		TreeMap<String,Integer> result = new TreeMap<String,Integer>();
+
+
 		if (filepath != null) {
 			try {
 				BufferedReader reader = new BufferedReader (new FileReader(filepath));
 				String line = reader.readLine();
-				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
+				while (line != null) {						//Verify it's not the end of the file.
+					String lowerCaseLine = line.toLowerCase();//Put all symptoms in lower case, to be sure there is not the same two times.
+					if (result.containsKey(lowerCaseLine))
+					{
+						result.replace(lowerCaseLine, result.get(lowerCaseLine)+1);//If symptom already exists, add one occurrence to this symptom in the result.
+					}
+					else if(!(lowerCaseLine.equals("")))//Verify the line is not empty.
+					{
+						result.put(lowerCaseLine,1);//If symptom doesn't exist, create a new symptom with one occurrence in the result.
+					}
+					line = reader.readLine();//read the following line.
 				}
 				reader.close();
 			} catch (IOException e) {
-				e.printStackTrace();
+				System.out.println("The specified file cannot be read. Please verifiy its existence and its location.");
 			}
 		}
-		
+
+
 		return result;
 	}
+
 
 }
