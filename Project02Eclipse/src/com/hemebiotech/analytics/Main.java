579d5e31afbaf989a813f5c91480ebc5b73b6cb3
andjamba <aurelien.ndjamba@gmail.com>
2021-01-04 14:44:11 +0100

    Creation de la classe Main éxécutant le programme

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,27 @@
+package com.hemebiotech.analytics;
+
+import java.util.ArrayList;
+import java.util.Map;
+//import java.util.TreeMap;
+
+/**
+ * Class representing count of the occurrence of the words 
+ * of a txt file and to export it to a result.out file 
+ * by classifying the ailments in alphabetical order. 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		String filepath = "Symptoms.txt";
+		
+		ReadSymptomDataFromFile symptomDataFromFile = new ReadSymptomDataFromFile();
+		ArrayList<String> listSymptomWithDuplicat = symptomDataFromFile.getSymptomsWithDuplicate(filepath);
+		Map<String, Integer> symptomsRate = symptomDataFromFile.getSymptomsRate(listSymptomWithDuplicat);
+		
+		WriteSymptomRateFromFile dataFromFile = new WriteSymptomRateFromFile();
+		dataFromFile.writerSymptomsToFile(symptomsRate);
+
+	}
+
+}
