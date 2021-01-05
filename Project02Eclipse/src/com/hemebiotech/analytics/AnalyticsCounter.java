b4950402d9552078fb4181a3616c85690513961c
Nicolas BIANCUCCI <nico5310@gmail.com>
2021-01-05 10:45:23 +0100

    Traitement de plusieurs erreurs d'orthographe - finalisation instruction else if

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,38 +5,41 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0; // initialize to 0
-	private static int rashCount = 0; // initialize to 0
-	private static int pupilCount = 0; // initialize to 0
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
+	private static int another = 0; // ajout d'une constante pour conpter le nombre d'autre symptomes
 
 	public static void main(String args[]) throws Exception {
 		// first get input
 		BufferedReader reader = new BufferedReader(new FileReader("Project02Eclipse/Symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0; // set i to 0
-		int headCount = 0; // counts headaches
-		while (line != null) {
-			i++; // increment i
+		while (line != null) { // permet de verifier que la variable "line" n'est pas nulle
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			} else if (line.equals("rush")) {
+				headacheCount++;
+				System.out.println("number of headaches: " + headacheCount);
+			} else if (line.equals("rash")) {
 				rashCount++;
 			} else if (line.contains("pupils")) {
 				pupilCount++;
+			} else { // Ajout de "else" afin de finaliser l'instruction else if et compter le nombre
+						// de symtomes non traités
+				another++;
 			}
 
 			line = reader.readLine(); // get another symptom
 		}
 		reader.close();
-		
+
 		// next generate output
 		FileWriter writer = new FileWriter("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.write("another symptoms : " + another + "\n"); // Ajout d'une ligne d'écriture afin de remonter le nombre
+																// d'autre symptomes non traités
 		writer.close();
 	}
-}""
+}
