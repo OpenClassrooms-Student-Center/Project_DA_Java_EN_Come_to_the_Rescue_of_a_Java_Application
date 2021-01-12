216386a96278db6aa3a18022e3b790b303f88a60
Estéban Savalle <59865189+estbn-sve@users.noreply.github.com>
2021-01-12 14:53:40 +0100

    Passage d'un tableau a un conteneur

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,24 +1,53 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 import java.lang.String;
+import java.util.stream.Collectors;
 
 
 public class AnalyticsCounter {
-    private static int headacheCount = 0;
-    private static int rashCount = 0;
-    private static int dialatedpupilCount = 0;
     public static int i = 0;
 
     public static void main(String args[]) throws Exception {
         // first get input
+        // changer de conteneur
+            BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+            HashMap<String, Integer> symptoms = new HashMap<>();
+            reader.lines().forEach(readLine -> {
+                if (symptoms.containsKey(readLine)) {
+                    symptoms.put(readLine, symptoms.get(readLine) + 1);
+                } else {
+                    symptoms.put(readLine, 1);
+                }
+            }
+        /*class AddSymptomsToContenair {
+            BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+            ArrayList<String> symptoms = reader.lines().collect(Collectors.toCollection(ArrayList::new));
+            System.out.println(symptoms);
+        }*/
+
+
+    //}
+
+
+        System.out.println(symptoms.toString());
+
+        // next generate output
+/*      FileWriter writer = new FileWriter("result.out");
+        writer.write("headache: " + headacheCount + "\n");
+        writer.write("rash: " + rashCount + "\n");
+        writer.write("dialated pupils: " + dialatedpupilCount + "\n");
+        writer.close();*/
+
+    }
+
+
+    public void func() throws Exception {
+        // first get input
         BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
         String line = reader.readLine();
-
-        while (line != null) {
+        /*while (line != null) {
         	System.out.println("symptom from file: " + line);
         	if (line.equals("headache")) {
         		headacheCount++;
@@ -30,39 +59,45 @@ public class AnalyticsCounter {
         		dialatedpupilCount++;
         	}
         	line = reader.readLine();
-        }
+        }*/
         int y = 100;
         String arraysymptoms[] = new String[y];
         String result;
 
         while (line != null) {
             arraysymptoms[i] = line;
-            result = arraysymptoms[i];
+            /*result = arraysymptoms[i];
             System.out.println("symptom from file: " + line);
-            System.out.println("Symptom add to array: " + result);
+            System.out.println("Symptom add to array: " + result);*/
             i++;
             line = reader.readLine();
         }
 
         // si symptoms es = alors ++ dans 2e array
+
+        for(String value : arraysymptoms)
+        {
+            System.out.println(value);
+        }
+
         i = 0;
         int resultint;
         int numbersymptoms[]= new int[y];
         int a = i;
-        for (int x=0; x<=y; x++ ){
-            if (arraysymptoms[i] == arraysymptoms[a]){
-                numbersymptoms[i]=numbersymptoms[i] + 1;
+        int cmp =0;
+        for (int x = 0; x < arraysymptoms.length; x++ ){
+            if (arraysymptoms[i] == arraysymptoms[x]){
+                cmp++;
+                numbersymptoms[i]= cmp;
                 resultint = numbersymptoms[i];
-                System.out.println(resultint);
+                System.out.println("yes occurence");
+                System.out.println("count occurence " + resultint);
+                a++;
+            }
+            else if (arraysymptoms[i] == arraysymptoms[x]){
+                System.out.println("no occurence");
                 a++;
             }
         }
-
-        // next generate output
-        FileWriter writer = new FileWriter("result.out");
-        writer.write("headache: " + headacheCount + "\n");
-        writer.write("rash: " + rashCount + "\n");
-        writer.write("dialated pupils: " + dialatedpupilCount + "\n");
-        writer.close();
     }
 }
