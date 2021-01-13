0a4951a502993f8f0dfa517cd749d162517cb197
Estéban Savalle <59865189+estbn-sve@users.noreply.github.com>
2021-01-13 20:58:14 +0100

    Application rendu dynamique

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -7,89 +7,43 @@ import java.util.stream.Collectors;
 
 
 public class AnalyticsCounter {
-    public static int i = 0;
+
 
     public static void main(String args[]) throws Exception {
         // first get input
-        // changer de conteneur
-            BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-            HashMap<String, Integer> symptoms = new HashMap<>();
-            reader.lines().forEach(readLine -> {
-                if (symptoms.containsKey(readLine)) {
-                    symptoms.put(readLine, symptoms.get(readLine) + 1);
-                } else {
-                    symptoms.put(readLine, 1);
+        BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+
+        ArrayList<String> symptomslist = new ArrayList<>();
+        reader.lines().forEach(readLine -> {
+            symptomslist.add(readLine);
+        });
+        Set<String> setsymptoms = new HashSet<>(symptomslist); // HashSet to delete double.
+        ArrayList<String> symptomslist2 = new ArrayList<>(setsymptoms); // List with not double to compare to count.
+        String[][] Symptoms = new String[symptomslist2.size()][2];
+
+        for (int i = 0; i < symptomslist2.size(); i++) {
+            Collections.sort(symptomslist2);
+            String result1 = symptomslist2.get(i);
+            int count = 0;
+            for (int a = 0; a < symptomslist.size(); a++) {
+                String result2 = symptomslist.get(a);
+                if (result1.equals(result2)) {
+                    count++;
                 }
             }
-
-
-        System.out.println(symptoms.toString());
+            String str1 = Integer.toString(count);
+            Symptoms[i][0] = result1;
+            Symptoms[i][1] = str1;
+        }
 
         // next generate output
-/*      FileWriter writer = new FileWriter("result.out");
-        writer.write("headache: " + headacheCount + "\n");
-        writer.write("rash: " + rashCount + "\n");
-        writer.write("dialated pupils: " + dialatedpupilCount + "\n");
-        writer.close();*/
-
-    }
-
-
-    public void func() throws Exception {
-        // first get input
-        BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-        String line = reader.readLine();
-        /*while (line != null) {
-        	System.out.println("symptom from file: " + line);
-        	if (line.equals("headache")) {
-        		headacheCount++;
-        	}
-        	else if (line.equals("rash")) {
-        		rashCount++;
-        	}
-        	else if (line.contains("dialated pupils")) {
-        		dialatedpupilCount++;
-        	}
-        	line = reader.readLine();
-        }*/
-        int y = 100;
-        String arraysymptoms[] = new String[y];
-        String result;
-
-        while (line != null) {
-            arraysymptoms[i] = line;
-            /*result = arraysymptoms[i];
-            System.out.println("symptom from file: " + line);
-            System.out.println("Symptom add to array: " + result);*/
-            i++;
-            line = reader.readLine();
-        }
-
-        // si symptoms es = alors ++ dans 2e array
-
-        for(String value : arraysymptoms)
-        {
-            System.out.println(value);
-        }
-
-        i = 0;
-        int resultint;
-        int numbersymptoms[]= new int[y];
-        int a = i;
-        int cmp =0;
-        for (int x = 0; x < arraysymptoms.length; x++ ){
-            if (arraysymptoms[i] == arraysymptoms[x]){
-                cmp++;
-                numbersymptoms[i]= cmp;
-                resultint = numbersymptoms[i];
-                System.out.println("yes occurence");
-                System.out.println("count occurence " + resultint);
-                a++;
-            }
-            else if (arraysymptoms[i] == arraysymptoms[x]){
-                System.out.println("no occurence");
-                a++;
+        FileWriter writer = new FileWriter("result.out");
+        for (int i = 0; i< Symptoms.length ; i++){
+            String result = Symptoms[i][0];
+            if (result != null ){
+                writer.write("Symptoms : " + Symptoms[i][0] + " occurrences numbers " + Symptoms[i][1] + "." + "\n");
             }
         }
+        writer.close();
     }
 }
