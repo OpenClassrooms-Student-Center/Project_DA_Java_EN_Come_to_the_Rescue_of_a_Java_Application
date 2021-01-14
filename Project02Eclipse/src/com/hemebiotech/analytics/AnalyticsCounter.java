02991e597c4d2ddc29096d9f8ac04ca46c20a04f
Nicolas BIANCUCCI <nico5310@gmail.com>
2021-01-14 15:24:56 +0100

    modification 1 du fichier AnalyticsCounter - Ajout BufferedReader + Map

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,44 +2,37 @@ package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	private static int another = 0; // ajout d'une constante pour conpter le nombre d'autre symptomes
 
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader(new FileReader("Project02Eclipse/Symptoms.txt"));
-		String line = reader.readLine();
+	public static void main(String[] args) throws IOException {
 
-		while (line != null) { // permet de verifier que la variable "line" n'est pas nulle
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			} else if (line.equals("rash")) {
-				rashCount++;
-			} else if (line.contains("pupils")) {
-				pupilCount++;
-			} else { // Ajout de "else" afin de finaliser l'instruction else if et compter le nombre
-						// de symtomes non traités
-				another++;
+		try {
+
+			// read file symptoms
+			BufferedReader reader = new BufferedReader(new FileReader("Project02Eclipse/symptoms.txt"));
+			String line = reader.readLine();
+
+			Map<String, Integer> symptomOccurrence = new HashMap<>();
+
+			while (line != null) {
+				line = reader.readLine();
+				if (symptomOccurrence.containsKey(line)) {
+					symptomOccurrence.put(line, symptomOccurrence.get(line) + 1);
+				} else {
+					symptomOccurrence.put(line, 1);
+				}
 			}
+			reader.close();
 
-			line = reader.readLine(); // get another symptom
+			System.out.println(symptomOccurrence.toString());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
 		}
-		reader.close();
-
-		// next generate output
-		FileWriter writer = new FileWriter("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.write("another symptoms : " + another + "\n"); // Ajout d'une ligne d'écriture afin de remonter le nombre
-																// d'autre symptomes non traités
-		writer.close();
 	}
 }
