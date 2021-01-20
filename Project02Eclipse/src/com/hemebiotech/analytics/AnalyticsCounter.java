bcb5124ab299b28055254dddd444e507b7be89ce
damien9372 <damien.foucher93@gmail.com>
2021-01-20 18:07:30 +0100

    20 janvier 2021 out.println + ecriture dans un fichier premier jet

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -22,24 +22,29 @@ public class AnalyticsCounter {
 		int i = 0; // set i to 0
 		// FileWriter writer2 = new FileWriter("result2.out");
 		// writer2.write("symptom" + line + "\n");
+		FileWriter writermap = new FileWriter("resulta.out");
 
 		while (line != null) {
 			i++; // increment i
 			System.out.println("symptom from file: " + line + " " + i);
 			// statut = false;
+
 			if (treemapsymptom.containsKey(line)) {
 
 				Integer mavaleur = (Integer) treemapsymptom.get(line);
 				Integer neovaleur = Integer.valueOf(mavaleur.intValue() + 1);
+				System.out.println("symptom existant: " + line + " " + neovaleur);
 				treemapsymptom.replace(line, neovaleur);
-
+				mapvalue = neovaleur;
 			} else {
 				treemapsymptom.put(line, Integer.valueOf(1));
 				System.out.println("symptom inexistant: " + line + " " + Integer.valueOf(1));
+				mapvalue = Integer.valueOf(1);
 			}
 			System.out.println("symptom actuel from file: " + mapkey);
-			FileWriter writermap = new FileWriter("resulta.out");
-			writermap.write("headache: " + mapkey + " " + mapvalue + "  " + i + "\n");
+			// FileWriter writermap = new FileWriter("resulta.out");
+			writermap.write("headache: " + line + " " + mapvalue + " un " + i + "\n");
+			// writermap.write("headache: " + mapkey + " " + mapvalue + " un " + i + "\n");
 			if (line.equals("headache")) {
 				headacheCount++;
 				System.out.println("number of headaches: " + headacheCount);
@@ -63,12 +68,22 @@ public class AnalyticsCounter {
 		// Afficher le contenu du MAP
 
 		FileWriter writer = new FileWriter("result.out");
-		writer.write("headache: " + mapkey + " " + mapvalue + "  " + i + "\n");
-		writer.write("headache: " + headacheCount + i + "\n");
+		writer.write("headache: " + mapkey + " " + mapvalue + " hg " + i + "\n");
+		writer.write("headache: " + headacheCount + " " + i + "\n");
 		writer.write("headache: " + headacheCount + "\n");
 		// writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		writermap.close();
 		// writermap.close();
+		while (0 != treemapsymptom.size()) {
+			// while (null != treemapsymptom.f()) {NoSuchElementException
+			System.out.println(
+					"symptom : " + treemapsymptom.firstKey() + " " + treemapsymptom.get(treemapsymptom.firstKey()));
+			writer.write("symptom : " + treemapsymptom.firstKey() + " " + treemapsymptom.get(treemapsymptom.firstKey())
+					+ "\n");
+			treemapsymptom.remove(treemapsymptom.firstKey());
+
+		}
+		writer.close();
 	}
 }
