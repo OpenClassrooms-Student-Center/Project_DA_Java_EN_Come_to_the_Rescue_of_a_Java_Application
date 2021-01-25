b7825338bed79b40ab628cfc8896274684d5154a
Lucille <lucille.lvt@gmail.com>
2021-01-25 14:33:18 +0100

    interface 1

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomFile.java
@@ -0,0 +1,15 @@
+package com.hemebiotech.analytics;
+
+import java.io.IOException;
+
+/**
+ * Interface generating a file output from any source to any source
+ */
+public interface ISymptomFile {
+    
+    /**
+     * Generates the file from a map
+     * @throws IOException
+     */
+    public void getFileOutput() throws IOException;
+}
