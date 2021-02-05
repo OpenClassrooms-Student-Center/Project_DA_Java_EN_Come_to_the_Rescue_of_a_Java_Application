b01b650becd2fbc0f94b14e2cf413365537b910d
Vaalrhona <38322481+Vaalrhona@users.noreply.github.com>
2021-02-05 17:43:13 +0100

    Update ISymptomAnalysis.java

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomAnalysis.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomAnalysis.java
@@ -1,19 +1,12 @@
 package com.hemebiotech.analytics;
-
 import java.util.Map;
-
-
 /**
  * Interface counting the symptoms
  */
-
 public interface ISymptomAnalysis {
-
     /**
      * Method counting the symptoms
      * @return a map with each symptom an its occurrences
      */
-
     public Map<String, Integer> analyzeSymptoms();
-    
 }
