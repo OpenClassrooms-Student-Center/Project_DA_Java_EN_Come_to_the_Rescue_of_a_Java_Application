e3791e2999d4ceb2ffd3ef754a9eae3ad99f5750
Vaalrhona <38322481+Vaalrhona@users.noreply.github.com>
2021-02-05 18:20:14 +0100

    Update AnalyticsCounter.java

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,7 +4,7 @@ public class AnalyticsCounter {
 	public static void main(String args[]) throws Exception {
         ISymptomReader reader = new ReadSymptomDataFromFile("Project02Eclipse/src/com/hemebiotech/analytics/symptoms.txt");
         ISymptomAnalysis mapping = new SymptomMapping(reader.GetSymptoms());
-		ISymptomFile output = new SymptomOutput(mapping.analyzeSymptoms());		
+	ISymptomFile output = new SymptomOutput(mapping.analyzeSymptoms());		
 		//try catch to get the exceptions if something happens during the file creation
         try {
             output.getFileOutput();
