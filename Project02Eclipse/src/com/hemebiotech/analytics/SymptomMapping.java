f7bda97a7f51daac5a26a95da65e5d5940bd97bd
Vaalrhona <38322481+Vaalrhona@users.noreply.github.com>
2021-02-05 17:44:01 +0100

    Update SymptomMapping.java

--- Project02Eclipse/src/com/hemebiotech/analytics/SymptomMapping.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomMapping.java
@@ -1,26 +1,19 @@
 package com.hemebiotech.analytics;
-
 import java.util.ArrayList;
 import java.util.TreeMap;
-
 /**  
  * 
  * Organizes the symptoms alphabetically and counts the number of occurrences in the file.
  */
 public class SymptomMapping {
-    
     private ArrayList<String> symptoms = new ArrayList<String>();
     private TreeMap<String, Integer> orderedSymptoms = new TreeMap<String, Integer>();
-
     /**
      * 
      * @param symptoms an unordered list of all the symptoms appearing in the raw file.
      */
-    
     public SymptomMapping(ArrayList<String> symptoms){
-
         this.symptoms = symptoms;
-
     }
     /**
      *
@@ -28,26 +21,19 @@ public class SymptomMapping {
      */
     public TreeMap<String, Integer> analyzeSymptoms(){
         //TreeMap used because it automatically arranges the keys alphabetically.
-        
         //going through the full list
+        for
         for (int counter=0; counter<symptoms.size(); counter++){
-
             String readSymptom = symptoms.get(counter);
-
             //if the symptom that is being read is already present in the file, the number of occurrences gets incremented.
             if (orderedSymptoms.containsKey(readSymptom)){
-
                 int i = orderedSymptoms.get(readSymptom)+1;
                 orderedSymptoms.replace(readSymptom, i);
-
             }
             else {
-
                 orderedSymptoms.put(symptoms.get(counter), 1);
-
             }
         }
-
         return orderedSymptoms;
     }
 }
