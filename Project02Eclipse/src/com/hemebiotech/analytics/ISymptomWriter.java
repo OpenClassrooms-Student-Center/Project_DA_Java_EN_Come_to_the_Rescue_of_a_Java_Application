6c38ef427d70da2bf99c9bb9084e5583d43095f4
Mano1603 <76477227+Mano1603@users.noreply.github.com>
2021-02-09 12:20:21 +0100

    Creation of Interface ISYmptomWriter
    Creation of WriteData method

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -1,5 +1,18 @@
 package com.hemebiotech.analytics;
 
+import java.io.IOException;
+import java.util.Map;
+
 public interface ISymptomWriter {
+	
+	/**
+	 * Method signature that will writes the results of the SymptomsCount Class on a text file.
+	 * @param Map <String, Integer>
+	 * @return keys symptoms  and  nbOccurences values 
+	 */
+	
+	
+	public String writedata ( Map<String,Integer>mSympt) throws IOException;
+
 
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/WriteData.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteData.java
@@ -1,5 +1,54 @@
 package com.hemebiotech.analytics;
 
-public class WriteData {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;;
+
+public class WriteData implements ISymptomWriter {
+		/**
+		* create a Text file and edit it with the results of the Map mSympt.
+		* @param Map<String,Integer>mSympt a Map created in SymptomsCount class
+		* @return serialize Map entries in a text file.
+		* @throws IOException
+		*
+		*/
+		public String writedata (Map<String,Integer> mSympt) throws IOException  {
+
+
+
+		try {
+		FileWriter writer = new FileWriter ("C://Users/33671/Desktop/result.txt");
+		BufferedWriter bw = new BufferedWriter (writer);
+		//commence la sequence de la boucle pour une map
+		for(Entry<String, Integer> entry : mSympt.entrySet()) {
+
+		String key = entry.getKey();
+		Integer value = entry.getValue();
+		String line = key+"  : "+value+"\n";
+
+		bw.write(line);
+		}
+
+
+		bw.close();
+		writer.close();
+		}catch (IOException e) {
+		e.printStackTrace();
+		}
+		return "";
+
+
+		}
+
+
+
+
+
+
+
+
+
 
 }
