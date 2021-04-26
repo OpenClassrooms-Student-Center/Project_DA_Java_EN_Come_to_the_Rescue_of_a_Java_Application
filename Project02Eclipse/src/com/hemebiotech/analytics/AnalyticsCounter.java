8e6c8e6980e87f686e6a55e13e557fc6e3a1490b
Khabibulix <80721211+Khabibulix@users.noreply.github.com>
2021-04-26 18:50:41 +0200

    Getter and setter tested.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,31 +1,29 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-
 public class AnalyticsCounter{
-	private static int headacheCount = 0;	
-	private static int rashCount = 0;		
-	private static int pupilCount = 0;
+	
 
 		
-	public static void getThatLine(String lineGet, int count){
-		if (lineGet.equals("headache")) {
-			System.out.println("Réussite");
-			count++;
-		} else {
-			System.out.println("échec");
-		}
+	public static void main(String args[]) throws Exception {
+		SymptomClass headAche = new SymptomClass("headAches", 0);
+		SymptomClass rash = new SymptomClass("rashes", 0);
+		SymptomClass dialatedPupils = new SymptomClass("dialated pupils", 0);
+	
+		System.out.println(SymptomClass.getSymptoms(rash));
+		System.out.println(SymptomClass.getSymptoms(headAche));
+		System.out.println(SymptomClass.getSymptoms(dialatedPupils));
+				
 		
 	}
+}
+		/**
+			private static int headacheCount = 0;	
+			private static int rashCount = 0;		
+			private static int pupilCount = 0;
 	
-	public static void main(String args[]) throws Exception {
-		// first get input
-				BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-				String line = reader.readLine();
-				
-		while (line != null) {
+		 	BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+			String line = reader.readLine();	
+		  	while (line != null) {
 			if (line.equals("headache")) {
 				headacheCount++;
 				System.out.println("number of headaches: " + headacheCount);
@@ -39,17 +37,9 @@ public class AnalyticsCounter{
 				System.out.println("number of pupil counts: " + pupilCount);
 			}
 
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine();	// get another symptom					
+			reader.close();		
 		}
-		getThatLine("headache", headacheCount);
+		**/
 		
-		
-		reader.close();		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-			writer.close();
-	}
-}
+
