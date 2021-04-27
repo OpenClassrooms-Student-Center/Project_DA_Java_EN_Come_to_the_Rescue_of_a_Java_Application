f69d58be3f233a160c8303f123628a758d107ff5
Khabibulix <80721211+Khabibulix@users.noreply.github.com>
2021-04-27 03:26:53 +0200

    Adding ArrayList for better gestion.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,45 +1,51 @@
 package com.hemebiotech.analytics;
 
-public class AnalyticsCounter{
-	
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
 
+public class AnalyticsCounter {
+	
+	private static int headacheCount = 0;	// initialize to 0
+	private static int rashCount = 0;		// initialize to 0
+	private static int pupilCount = 0;		// initialize to 0
 		
 	public static void main(String args[]) throws Exception {
-		SymptomClass headAche = new SymptomClass("headAches", 0);
-		SymptomClass rash = new SymptomClass("rashes", 0);
-		SymptomClass dialatedPupils = new SymptomClass("dialated pupils", 0);
-	
-		System.out.println(SymptomClass.getSymptoms(rash));
-		System.out.println(SymptomClass.getSymptoms(headAche));
-		System.out.println(SymptomClass.getSymptoms(dialatedPupils));
-				
 		
-	}
-}
-		/**
-			private static int headacheCount = 0;	
-			private static int rashCount = 0;		
-			private static int pupilCount = 0;
-	
-		 	BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-			String line = reader.readLine();	
-		  	while (line != null) {
+		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		String line = reader.readLine();
+		ArrayList<String> logSymptomes = new ArrayList<String>();
+				
+		while (line != null) {
+			line = reader.readLine();
+			logSymptomes.add(line);		
+			System.out.println(logSymptomes);
+		}
+		
+		
+		/**while (line != null) {
+			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
 				headacheCount++;
 				System.out.println("number of headaches: " + headacheCount);
 			}
 			else if (line.equals("rash")) {
 				rashCount++;
-				System.out.println("number of rashes: " + rashCount);
 			}
-			else if (line.equals("dialated pupils")) {
+			else if (line.equals("pupils")) {
 				pupilCount++;
-				System.out.println("number of pupil counts: " + pupilCount);
-			}
+			}**/
 
-			line = reader.readLine();	// get another symptom					
-			reader.close();		
-		}
-		**/
+			
 		
-
+		reader.close();
+		
+		// next generate output
+		FileWriter writer = new FileWriter ("result.out");
+		writer.write("headache: " + headacheCount + "\n");
+		writer.write("rash: " + rashCount + "\n");
+		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.close();
+	}
+}
