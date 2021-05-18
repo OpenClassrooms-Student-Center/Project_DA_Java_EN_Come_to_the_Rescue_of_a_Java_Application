f7352bc713353f43c7c2bcdd4d29a3ffa44252d1
w-boar <av.w.boar@gmail.com>
2021-05-18 11:08:32 +0200

    reading (class & interface)

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -4,19 +4,19 @@ import java.util.List;
 
 /**
  * @author Caroline
- * @version 1.0
- * Anything that will read symptom data from a source
- * The important part is, the return value from the operation, which is a list of strings,
- * that may contain many duplications
+ * @version 1.0 Anything that will read symptom data from a source The important
+ *          part is, the return value from the operation, which is a list of
+ *          strings, that may contain many duplications
  * 
- * The implementation does not need to order the list
+ *          The implementation does not need to order the list
  * 
  */
 public interface ISymptomReader {
 	/**
 	 * If no data is available, return an empty List
 	 * 
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+	 * @return a raw listing of all Symptoms obtained from a data source, duplicates
+	 *         are possible/probable
 	 */
-	List<String> getSymptoms ();
+	List<String> getSymptoms();
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -6,53 +6,48 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 /**
- *@author Caroline
- *@version 1.0
-  *copy data from a file (: 1 string per line) without any treatment (unordered, duplications,...)
+ * @author Caroline
+ * @version 1.0 copy data from a file (: 1 string per line) without any
+ *          treatment (unordered, duplications,...)
  */
-public class ReadSymptomDataFromFile 
-{
-/**
- *@param filepath
- *: filepath of a list of symptoms
- **/
-        private String filepath;
+public class ReadSymptomDataFromFile {
+	/**
+	 * @param filepath : filepath of a list of symptoms
+	 **/
+	private String filepath;
 
-        public ReadSymptomDataFromFile(String filepath)
-        {
-                this.filepath = filepath;
-        }
-/**
- * read data from a file (: 1 string per line) an add them one by one to a ArrayList
- * @return ArrayList<String>
- */
-        public ArrayList <String> getSymptoms()
-        {
-                ArrayList <String> symptomList = new ArrayList<String>();
+	public ReadSymptomDataFromFile(String filepath) {
+		this.filepath = filepath;
+	}
 
-                if (filepath != null)
-                {        
-                    try
-                    {
-                    BufferedReader reader = new BufferedReader(new FileReader(filepath));
-                    String symptom = reader.readLine();
+	/**
+	 * read data from a file (: 1 string per line) an add them one by one to a
+	 * ArrayList
+	 * 
+	 * @return ArrayList<String>
+	 */
+	public ArrayList<String> getSymptoms() {
+		ArrayList<String> symptomList = new ArrayList<String>();
 
-                    while (symptom != null)
-                    {
-                        symptomList.add(symptom);
-                        symptom = reader.readLine();
-                    }
-                    reader.close();
-                    }
-/**
- *  *@throws IOException
- */
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
+		if (filepath != null) {
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(filepath));
+				String symptom = reader.readLine();
 
-                return symptomList;
-        }
-} 
+				while (symptom != null) {
+					symptomList.add(symptom);
+					symptom = reader.readLine();
+				}
+				reader.close();
+			}
+			/**
+			 * *@throws IOException
+			 */
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return symptomList;
+	}
+}
