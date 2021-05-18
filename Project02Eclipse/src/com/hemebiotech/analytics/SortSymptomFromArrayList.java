6b7b299799078681b0ac45814d7436f01f9b33ad
w-boar <av.w.boar@gmail.com>
2021-05-18 11:17:27 +0200

    Sorting (class)

--- Project02Eclipse/src/com/hemebiotech/analytics/SortSymptomFromArrayList.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SortSymptomFromArrayList.java
@@ -4,30 +4,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.TreeMap;
 
-public class SortSymptomFromArrayList 
-{
+public class SortSymptomFromArrayList {
 
-	private ArrayList <String> symptomList = new ArrayList<String>();
-	
+	private ArrayList<String> symptomList = new ArrayList<String>();
+
 	/**
 	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 * @param filepath a full or partial path to file with symptom strings in it,
+	 *                 one per line
 	 */
-	public SortSymptomFromArrayList (ArrayList <String> symptomList) 
-	{
+	public SortSymptomFromArrayList(ArrayList<String> symptomList) {
 		this.symptomList = symptomList;
-    }
-	
-	public TreeMap<String,Integer> sortSymptom()
-	{
-		TreeMap<String,Integer> symptomAndOccurrencesMap = new TreeMap<String,Integer>();
-		
-		if (symptomList != null)
-		{
-			for (String elt : symptomList)
-			{
-				if (!symptomAndOccurrencesMap.containsKey(elt))
-				{
+	}
+
+	public TreeMap<String, Integer> sortSymptom() {
+		TreeMap<String, Integer> symptomAndOccurrencesMap = new TreeMap<String, Integer>();
+
+		if (symptomList != null) {
+			for (String elt : symptomList) {
+				if (!symptomAndOccurrencesMap.containsKey(elt)) {
 					symptomAndOccurrencesMap.put(elt, Collections.frequency(symptomList, elt));
 				}
 			}
