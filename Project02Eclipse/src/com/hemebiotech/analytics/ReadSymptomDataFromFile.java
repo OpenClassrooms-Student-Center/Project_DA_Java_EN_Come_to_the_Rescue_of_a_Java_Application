42cd0d5bce5cfcc94e4e7d3ae92724dfc4ae6d1b
chyadr <chaimaafakir@gmail.com>
2021-06-20 20:57:51 +0200

    [ocr] - code refactoring+javadoc+adding methods counting/sorting/saving

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -15,7 +15,7 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	// This might be final because the initial value will keep the same
 	// Rename field from filepath  to filePath by following java convention naming
-	private final String filePath;  // Private String filepath;
+	private final String filePath;
 
 	/**
 	 * @param filePath: A full or partial path to file with symptom strings in it, one per line
@@ -32,23 +32,19 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 		// for example if we have 2 variables of List(interface) type, Arraylist(class) and LinkedList(class), we can affect the result
 		// of each one to the other, Else we will have a compilation error
 		// I removed type<String> from instantiation part, because it's useful (proposed by Intellij)
-		List<String> result = new ArrayList<>();//ArrayList<String> result = new ArrayList<String>();
+		List<String> result = new ArrayList<>();
 
 
 		if (filePath != null) {
 			System.out.println("**************************************** Start Reading from file ***********************************************");
 			// Methode 1/ I added a try with resources in order to close the resource automatically
-			// Methode 2/ we can add a finally after catch
-			try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {    //try {
-				// class BuffredReader in order to read the file and add it to the memory																			// BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				// https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
+			try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+				// class BuffredReader in order to read the file and add it to the memory
 				// refactor code : readLine from file and test if it's != null in the same time (same code bloc)
-				String line;                                    // String line = reader.readLine();
-				while ((line = reader.readLine()) != null) {    // while (line != null) {
-					result.add(line);                            // result.add(line);
-					// line = reader.readLine();
+				String line;
+				while ((line = reader.readLine()) != null) {
+					result.add(line);
 				}
-				//reader.close();
 			} catch (IOException e) {
 				System.out.println("Error occuring during reading from file, please contact your administrator, for more details : " + e.getMessage());
 			}
