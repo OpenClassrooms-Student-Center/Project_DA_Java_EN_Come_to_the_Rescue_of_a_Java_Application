9c1023aab177a539dff19290bb4ea54d78e529cf
chyadr <chaimaafakir@gmail.com>
2021-06-20 20:48:40 +0200

    [ocr] - code refactoring+javadoc+adding methods counting/sorting/saving

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,87 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Occurrences counting,sorting,and saving into file part
+ */
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+    private static final String filePathReader = "Project02Eclipse\\symptoms.txt";
+    private static final String filePathWriter = "Project02Eclipse\\results.out.txt";
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+    /**
+     * Implementing a main method in order to execute the program
+     *
+     * @param args Are params that can be provided in our method(not provided in our case)
+     */
+    public static void main(String args[]) {
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+        ISymptomReader reader = new ReadSymptomDataFromFile(filePathReader);
+        List<String> symptoms = reader.getSymptoms();
+        Map<String, Integer> mapOccurrences = analyticsCounter.countOccurrences(symptoms);
+        Map<String, Integer> sortedMapOccurrences = analyticsCounter.sorting(mapOccurrences);
+        analyticsCounter.saving(sortedMapOccurrences);
+    }
+
+    /**
+     * This method is charged to count the symptoms occurrences
+     *
+     * @param symptoms All symptoms that we can read from file
+     * @return Return symptoms with their numbers of occurrences
+     */
+    private Map<String, Integer> countOccurrences(List<String> symptoms) {
+        System.out.println("**************************************** Start counting from List ***********************************************");
+        Map<String, Integer> mapOccurrences = new HashMap<>();
+        for (String symptom : symptoms) {
+            mapOccurrences.put(symptom, mapOccurrences.getOrDefault(symptom, 0) + 1);
+        }
+        System.out.println("**************************************** End counting from List ***********************************************");
+        return mapOccurrences;
+
+    }
+
+    /**
+     * Method that aims to sort the symptoms in alphabetical order from A to Z
+     *
+     * @param mapOccurrences All symptoms and their number of occurrences
+     * @return Symptoms sorted by alphabetical order from A to Z with their number of occurrences
+     */
+    private Map<String, Integer> sorting(Map<String, Integer> mapOccurrences) {
+        System.out.println("**************************************** Start sorting from Map ***********************************************");
+        Map<String, Integer> sortedMapOccurrences = mapOccurrences.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+        System.out.println("**************************************** End sorting from Map ***********************************************");
+        return sortedMapOccurrences;
+    }
+
+    /**
+     * Method that is responsible for writing the symptoms with their number of occurrences into file
+     *
+     * @param mapOccurrences All symptoms and their number of occurrences
+     */
+    private void saving(Map<String, Integer> mapOccurrences) {
+        System.out.println("**************************************** Start saving into file ***********************************************");
+        //BufferedWriter in order to  write the occurrences coming  from memory to results.out.txt file
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(filePathWriter), "utf-8"))) {
+
+            for (Map.Entry<String, Integer> symptomsOccurrence : mapOccurrences.entrySet()) {
+                writer.write(symptomsOccurrence.getKey() + ":" + symptomsOccurrence.getValue());
+                writer.newLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error occurring during saving into file, please contact your administrator, for more details : " + e.getMessage());
+        }
+        System.out.println("**************************************** End saving into file ***********************************************");
+    }
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -12,35 +12,50 @@ import java.util.List;
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
-	private String filepath;
-	
+
+	// This might be final because the initial value will keep the same
+	// Rename field from filepath  to filePath by following java convention naming
+	private final String filePath;  // Private String filepath;
+
 	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
+	 * @param filePath: A full or partial path to file with symptom strings in it, one per line
 	 */
-	public ReadSymptomDataFromFile (String filepath) {
-		this.filepath = filepath;
+	public ReadSymptomDataFromFile(String filePath) {
+		this.filePath = filePath;
 	}
-	
+
 	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
-				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
+	// rename from GetSymptoms to getSymptoms by following java convention name
+	public List<String> getSymptoms() {
+		// https://stackoverflow.com/questions/7716435/why-would-you-declare-an-interface-and-then-instantiate-an-object-with-it-in-jav
+		// Coding to an interface, long term
+		// for example if we have 2 variables of List(interface) type, Arraylist(class) and LinkedList(class), we can affect the result
+		// of each one to the other, Else we will have a compilation error
+		// I removed type<String> from instantiation part, because it's useful (proposed by Intellij)
+		List<String> result = new ArrayList<>();//ArrayList<String> result = new ArrayList<String>();
+
+
+		if (filePath != null) {
+			System.out.println("**************************************** Start Reading from file ***********************************************");
+			// Methode 1/ I added a try with resources in order to close the resource automatically
+			// Methode 2/ we can add a finally after catch
+			try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {    //try {
+				// class BuffredReader in order to read the file and add it to the memory																			// BufferedReader reader = new BufferedReader (new FileReader(filepath));
+				// https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
+				// refactor code : readLine from file and test if it's != null in the same time (same code bloc)
+				String line;                                    // String line = reader.readLine();
+				while ((line = reader.readLine()) != null) {    // while (line != null) {
+					result.add(line);                            // result.add(line);
+					// line = reader.readLine();
 				}
-				reader.close();
+				//reader.close();
 			} catch (IOException e) {
-				e.printStackTrace();
+				System.out.println("Error occuring during reading from file, please contact your administrator, for more details : " + e.getMessage());
 			}
+
+			System.out.println("**************************************** End Reading from file ***********************************************");
 		}
-		
+
 		return result;
 	}
 
