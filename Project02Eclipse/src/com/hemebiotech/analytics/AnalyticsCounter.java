24fc10794754591475682ea842876fecccf31c27
chyadr <chaimaafakir@gmail.com>
2021-06-21 19:03:35 +0200

    [ocr] - add java comments to help reading code

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,6 +5,8 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 /**
@@ -21,10 +23,15 @@ public class AnalyticsCounter {
      * @param args Are params that can be provided in our method(not provided in our case)
      */
     public static void main(String args[]) {
+        // Create new instance of AnalyticsCounter
         AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+        // Create a new instance of ReadSymptomDataFromFile
         ISymptomReader reader = new ReadSymptomDataFromFile(filePathReader);
+        // Get all symptoms from file as a list
         List<String> symptoms = reader.getSymptoms();
+        // Count symptoms and their numbers sorted by alphabetical order
         Map<String, Integer> mapOccurrences = analyticsCounter.counting(symptoms);
+        // save result to a file
         analyticsCounter.saving(mapOccurrences);
     }
 
@@ -36,6 +43,7 @@ public class AnalyticsCounter {
      */
     private Map<String, Integer> counting(List<String> symptoms) {
         System.out.println("**************************************** Start counting from List ***********************************************");
+        // Use TreeMap in order to sort this map by alphabetical symptoms
         Map<String, Integer> mapOccurrences = new TreeMap<>();
         for (String symptom : symptoms) {
             mapOccurrences.put(symptom, mapOccurrences.getOrDefault(symptom, 0) + 1);
@@ -54,8 +62,8 @@ public class AnalyticsCounter {
         System.out.println("**************************************** Start saving into file ***********************************************");
         //BufferedWriter in order to  write the occurrences coming  from memory to results.out.txt file
         try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(filePathWriter), "utf-8"))) {
-
+                new FileOutputStream(filePathWriter), StandardCharsets.UTF_8))) {
+            
             for (Map.Entry<String, Integer> symptomsOccurrence : mapOccurrences.entrySet()) {
                 writer.write(symptomsOccurrence.getKey() + ":" + symptomsOccurrence.getValue());
                 writer.newLine();
