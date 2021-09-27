9c70c1450914de0bbdc925ccc2a992af9d8d997d
Didier Cotrel <didier.cotrel@gmail.com>
2021-09-27 19:05:45 +0200

    retrait de la sortie console

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,14 +1,9 @@
 package com.hemebiotech.analytics;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.TreeMap;
 
 /**
- * contains the main
- * 
  * @author didier
- *
  */
 public class AnalyticsCounter {
 
@@ -17,18 +12,9 @@ public class AnalyticsCounter {
 		/**
 		 * Call of the FileWriterData class to write to the file: "results.out".
 		 */
-
 		FileWriterData finalResult = new FileWriterData();
 		finalResult.writeSymptomsData();
 
-		/**
-		 * Call the TreeMap and display in the console to check that the output is good.
-		 */
-
-		ExtracSymptomData consoleOutput = new ExtracSymptomData();
-		TreeMap<String, Integer> symptoms = consoleOutput.extracSymptomsData();
-		for (Map.Entry<String, Integer> entry : symptoms.entrySet())
-			System.out.println("- " + entry.getKey() + ": " + entry.getValue());
 	}
 
 }
