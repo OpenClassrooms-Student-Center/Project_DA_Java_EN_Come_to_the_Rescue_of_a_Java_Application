5c6df256679b6c21ecee1d9e3f54ae9e51ac4b25
Didier Cotrel <didier.cotrel@gmail.com>
2021-09-28 18:34:25 +0200

    Instanciation des 3 Interface: ISymptomReader, Icomptage, IFileWriterData

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,6 +1,8 @@
 package com.hemebiotech.analytics;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.TreeMap;
 
 /**
  * @author didier
@@ -9,11 +11,15 @@ public class AnalyticsCounter {
 
 	public static void main(String[] args) throws IOException {
 
-		/**
-		 * Call of the FileWriterData class to write to the file: "results.out".
-		 */
-		FileWriterData finalResult = new FileWriterData();
-		finalResult.writeSymptomsData();
+		// extraction des données dans le fichier
+		ISymptomReader file = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
+		List<String> symptomsFile = file.getSymptoms();
+		// comptage des symptoms.
+		Icomptage icomptage = new Comptage();
+		TreeMap<String, Integer> symptomsCompte = icomptage.comptageSymptomsData(symptomsFile);
+		// Ecriture dans le fichier results.out
+		IFileWriterData iFileWriterData = new FileWriterData();
+		iFileWriterData.writeSymptomsData(symptomsCompte);
 
 	}
 
