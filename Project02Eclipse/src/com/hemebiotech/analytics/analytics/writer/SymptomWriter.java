f64b138f3d6f9432989a10008125a3320579fc24
SaithTime <50336969+SaithTime@users.noreply.github.com>
2021-10-12 12:58:15 +0200

    Create symptom Writter

--- Project02Eclipse/src/com/hemebiotech/analytics/analytics/writer/SymptomWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/analytics/writer/SymptomWriter.java
@@ -1,4 +1,33 @@
 package com.hemebiotech.analytics.analytics.writer;
 
-public class SymptomWriter {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.TreeMap;
+
+public class SymptomWriter implements ISymptomWriter{
+
+    private final String fileName; // Name of final file.
+
+    /**
+     *
+     * @param fileName a full or partial path to file with symptom strings in it, one per line
+     */
+    public SymptomWriter(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void write(TreeMap<String, Long> symptomsMap) {
+        try {
+            FileWriter writer = new FileWriter(this.fileName);
+            for (String symptoms : symptomsMap.keySet()) {
+                String toWrite = symptoms + ":" + symptomsMap.get(symptoms);
+                System.out.println(toWrite);
+                writer.write(toWrite + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
