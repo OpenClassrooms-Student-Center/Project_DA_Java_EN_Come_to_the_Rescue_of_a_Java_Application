0a02f8b8f89e9b58a7a4becc29f08dc7176ea270
Erwan Guiffant <ergu@mac.com>
2022-01-25 12:21:40 +0100

    Implémentation interface d'écriture

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,28 +1,20 @@
 package com.hemebiotech.analytics;
 
-import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class AnalyticsCounter {
 	
 	public static void main(String args[]) throws Exception {
 
-		// Liste des symptomes
+		// List of symptoms
 		ArrayList<String> listSymptomes = new ArrayList<String>();
 
-		// first get input
+		// Get input
 		ReadSymptomDataFromFile fichierLu = new ReadSymptomDataFromFile("./Project02Eclipse/symptoms.txt");
 		listSymptomes = fichierLu.GetSymptoms();
-
-		int i = 0;	// set i to 0
-
-		while (i < listSymptomes.size()) {
-			System.out.println("symptom from file: " + listSymptomes.get(i));
-			i++;
-		}
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.close();
+		// Generate output
+		WriteSymptomData fichierOut = new WriteSymptomData();
+		fichierOut.WriteSymptoms(listSymptomes);
 	}
 }
