7e20c420d9636cfe48a009fd3552ff96039ab8ab
Erwan Guiffant <ergu@mac.com>
2022-01-25 10:59:51 +0100

    Commit de test

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,9 +5,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headacheCount = 1; // initialize to 0
+	private static int rashCount = 1; // initialize to 0
+	private static int pupilCount = 1; // initialize to 0
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
