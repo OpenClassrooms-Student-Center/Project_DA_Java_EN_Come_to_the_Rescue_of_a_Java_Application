5d0bd0a291abf2667baeff3010dadade2db91a65
Erwan Guiffant <ergu@mac.com>
2022-01-28 09:01:04 +0100

    Classe de tri et meilleure gestion des erreurs

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,59 +1,24 @@
 package com.hemebiotech.analytics;
 
-import java.util.ArrayList;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class AnalyticsCounter {
 	
-	public static void main(String args[]) throws Exception {
+	public static final String INPUT_FILE = "./Project02Eclipse/symptoms.txt";
+	public static final String OUTPUT_FILE = "results.out";
 
-		// List of symptoms read from the file
-		ArrayList<String> listSymptomsIn = new ArrayList<String>();
-		// Working list of symptoms (sort and counted)
-		TreeMap<String, Integer> sortedListSymptoms = new TreeMap<String, Integer>();
-		// List of symptoms to write in the out file
-		ArrayList<String> listSymptomsOut = new ArrayList<String>();
+	public static void main(String args[]) {
 
-		// Get symptoms
-		ReadSymptomDataFromFile fichierLu = new ReadSymptomDataFromFile("./Project02Eclipse/symptoms.txt");
-		listSymptomsIn = fichierLu.GetSymptoms();
+		Map<String, Integer> listSymptomsOut;
 		
-		// Sort & count
-		sortedListSymptoms = SortAndCompt(listSymptomsIn);
-		listSymptomsOut = ListSymptomsSorted(sortedListSymptoms);
+		// Get, sort and count Input
+		SortSymptoms sortedSymptoms = new SortSymptoms();
+		listSymptomsOut = sortedSymptoms.sortAndCountSymptoms(INPUT_FILE);
 
-		// Generate output
-		WriteSymptomData fichierOut = new WriteSymptomData("./results.out");
+		// Generate output file
+		WriteSymptomData fichierOut = new WriteSymptomData(OUTPUT_FILE);
 		fichierOut.WriteSymptoms(listSymptomsOut);
+
 	};
-
-	public static TreeMap<String, Integer> SortAndCompt(ArrayList<String> listeSymptoms) {
-
-		TreeMap<String, Integer> result = new TreeMap<String, Integer>();
-		int nbOcc = 0;
-
-		for (int i = 0; i < listeSymptoms.size(); i++) {
-			nbOcc = result.getOrDefault(listeSymptoms.get(i), 0);
-			if (nbOcc == 0) {
-				result.put(listeSymptoms.get(i), 1);
-			} else {
-				result.replace(listeSymptoms.get(i), nbOcc + 1);
-			}
-		}
-		return result;
-	}
-
-	public static ArrayList<String> ListSymptomsSorted(TreeMap<String, Integer> listSorted) {
-
-		ArrayList<String> result = new ArrayList<String>();
-		String line = "";
-
-		for (Map.Entry<String, Integer> m : listSorted.entrySet()) {
-			line = m.getKey() + " : " + m.getValue();
-			result.add(line);
-		}
-		return result;
-	}
 
 }
