6c1c7f5018bf8c252bae7c31988f677ef9241414
GarlandNic <nicolas-garland@club-internet.fr>
2022-02-21 15:31:40 +0100

    Nettoyage et classe Symptom

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,36 +5,33 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 /**
- * to be filled later
- * structure of javadoc
+ * to be filled later structure of javadoc
  * 
  * 
  */
 public class AnalyticsCounter {
 	/**
-	 * to be filled later
-	 * structure of javadoc
-	 * produce what we want
+	 * to be filled later structure of javadoc produce what we want
 	 * 
 	 * @args blabla
 	 */
-	private static int headacheCount = 0;	// to record the number of headache
-	private static int rashCount = 0;		// to record the number of rash
-	private static int pupilCount = 0;		// to record the number of pupil
-	
+	private static int headacheCount = 0; // to record the number of headache
+	private static int rashCount = 0; // to record the number of rash
+	private static int pupilCount = 0; // to record the number of pupil
+
 	public static void main(String args[]) throws Exception {
 		/**
-		 * to be filled later
-		 * structure of javadoc
+		 * to be filled later structure of javadoc 
 		 * produce what we want
+		 * main steps: 1) read file (result: list of String) ; 2) 
 		 * 
 		 * @args blabla
 		 */
 
-		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
+		BufferedReader reader = new BufferedReader(new FileReader("Project02Eclipse/symptoms.txt"));
 		String line = reader.readLine(); // first read line
 
-		int i = 0;	// counter for the lines of the file
+		int i = 0; // counter for the lines of the file
 		int headCount = 0;
 		while (line != null) {
 			i++;
@@ -42,19 +39,17 @@ public class AnalyticsCounter {
 			if (line.equals("headache")) {
 				headCount++;
 				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
+			} else if (line.equals("rush")) {
 				rashCount++;
-			}
-			else if (line.contains("pupils")) {
+			} else if (line.contains("pupils")) {
 				pupilCount++;
 			}
 
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine(); // get another symptom
 		}
-		
+
 		// generate output
-		FileWriter writer = new FileWriter ("Project02Eclipse/result.out");
+		FileWriter writer = new FileWriter("Project02Eclipse/result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Symptom.java
@@ -0,0 +1,11 @@
+package com.hemebiotech.analytics;
+
+public class Symptom {
+	public int count ;
+	public String name ;
+	
+	public Symptom(String name) {
+		this.count = 1;
+		this.name = name;
+	}
+}
