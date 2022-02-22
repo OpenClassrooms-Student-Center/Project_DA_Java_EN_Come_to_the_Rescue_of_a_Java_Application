0a386ded461e711b660b8a8f05935695d6d442ef
S606590 <S606590@PO5CG013D1N8.siege.axa-fr.intraxa>
2022-02-22 16:42:12 +0100

    mise en place  classe SymptomCounter pour compter ordonner les symptomes

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,16 +1,17 @@
 package com.hemebiotech.analytics;
 
 import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
 
-	
 	public static void main(String args[]) throws Exception {
-		
-		
+
 		ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
 		List<String> listOfSymptom = readSymptomDataFromFile.getSymptoms();
-		
-		
+
+		SymptomCounter symptomCounter = new SymptomCounter();
+		Map<String, Integer> finalCount = symptomCounter.listToMap(listOfSymptom);
+
 	}
 }
