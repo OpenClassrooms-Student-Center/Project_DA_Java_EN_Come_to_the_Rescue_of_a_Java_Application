c7ade7e3bfc4f39076539f958a354901ac2e9ba7
S606590 <S606590@PO5CG013D1N8.siege.axa-fr.intraxa>
2022-02-23 16:41:39 +0100

    final delivery after checking

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -6,19 +6,19 @@ import java.util.Map;
 /**
  * @author DOLLOU 
  * @since 09/02/2021
- *  
+ * the main Class allows you to methodically extract/read,count/order, write in the final File path
  */
 
 public class AnalyticsCounter {
 	
 	final static String INPUT = "Project02Eclipse/symptoms.txt";
 	final static String OUTPUT = "Project02Eclipse/results.out";
-	
+	/**
+	 * the main method initialized the classes in three steps and apply their methods 
+	 */
 
 	public static void main(String args[]) throws Exception {
-/**
- * 
- */
+
 		ISymptomReader readSymptomDataFromFile = new ReadSymptomDataFromFile(INPUT);
 		List<String> listOfSymptom = readSymptomDataFromFile.getSymptoms();
 
