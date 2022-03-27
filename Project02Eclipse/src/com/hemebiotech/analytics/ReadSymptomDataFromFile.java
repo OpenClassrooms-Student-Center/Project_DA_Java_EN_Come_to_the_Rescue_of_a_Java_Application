6f157dbc73bfc296e829393a02321cf24dcf008e
FBRDE <100838760+FBRDE@users.noreply.github.com>
2022-03-27 08:15:44 +0200

    Correction du code
    Correction du code

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,6 +1,10 @@
 package com.hemebiotech.analytics;
 
+/**
+ * @author Fatima
+ */
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,35 +17,74 @@ import java.util.List;
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
-	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 */
-	public ReadSymptomDataFromFile (String filepath) {
-		this.filepath = filepath;
-	}
-	
-	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
-				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
-				}
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
+
+	public ReadSymptomDataFromFile() {
 	}
 
+	/**
+	 * @param filepath a full or partial path to file with symptom strings in it,
+	 *                 one per line
+	 */
+	public ReadSymptomDataFromFile(String filepath) {
+		this.filepath = filepath;
+	}
+
+	public String getFilepath() {
+		return filepath;
+	}
+
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+
+	/**
+	 * If no data is available, return an empty List
+	 * 
+	 * @return a raw listing of all Symptoms obtained from a data source, duplicates
+	 *         are possible/probable
+	 * @exception generate FileNotFoundException when file not found
+	 * @exception generate EmptyFileException when file is empty
+	 * @exception generate IOException when error occurred
+	 */
+	@Override
+	public List<String> getSymptoms() {
+		ArrayList<String> result = new ArrayList<String>();
+
+		if (filepath != null) {
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(filepath));
+				String line = reader.readLine();
+				if (line == null) {
+					reader.close();
+					throw new EmptyFileException();
+				}
+
+				while (line != null) {
+					if (!line.trim().equals(""))
+						result.add(line);
+					line = reader.readLine();
+				}
+
+				if (result.isEmpty()) {
+					reader.close();
+					throw new EmptyFileException();
+				}
+
+				reader.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("Read data from file: file not found!");
+				System.out.println("Read data from file: " + e.getMessage());
+			} catch (EmptyFileException e) {
+				System.out.println("Read data from file: " + e.getMessage());
+			}
+
+			catch (IOException e) {
+				System.out.println("Read data from file: an Error occured!");
+				e.printStackTrace();
+			}
+		} else
+			System.out.println("Read data from file: file path is null!");
+
+		return result;
+	}
 }
