f010420acbb52a66f2b041e5a959804e4466b02a
Samir Messbah <messbah.s@gmail.com>
2022-04-21 02:03:30 +0200

    Creation de la Map avec les symptoms sans doublon

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,19 +3,19 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
+/*
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
 	public static void main(String args[]) throws Exception {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
 		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
 		while (line != null) {
 			i++;	// increment i
 			System.out.println("symptom from file: " + line);
@@ -23,21 +23,45 @@ public class AnalyticsCounter {
 				headCount++;
 				System.out.println("number of headaches: " + headCount);
 			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
 			line = reader.readLine();	// get another symptom
 		}
 		
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
 	}
 }
+*/
+
+public class AnalyticsCounter {
+	public static void main(String args[]) throws Exception {
+		// first get input
+		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		String line = reader.readLine();
+		int	i = 0;
+		int j = 0;
+		boolean t = false;
+		Map<String, Integer> symptoms = new HashMap<String, Integer>();
+
+		while (line != null) {
+			if (symptoms.containsKey(line) == false) {
+				symptoms.put(line, 1);
+			}
+			else {
+				System.out.println("Le mot existe deja");
+				j = symptoms.get(line);
+				symptoms.put(line, j+1);
+				/* La valeur integrer correspondant a la clé de cette ligne soit egale a la valeur actuelle +1*/
+			}
+			j = 0;
+			line = reader.readLine();	// get another symptom
+		}
+
+		// next generate output
+		FileWriter writer = new FileWriter ("result.out");
+		writer.write(symptoms + "\n");
+		writer.close();
+	}
+}
+
