065b9149a6afe46bf32cc4835ac67c39ddf2a44e
Regis Eboa <regiseboa@gmail.com>
2022-05-04 19:37:56 +0200

    Création de la nouvelle classe Main.java

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,25 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile("/Users/ray/Documents/DeveloppeurApplicationJAVA/P2_debuggez_une_application_Java/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt");
+
+		List<String> listOfSymptoms = readSymptomDataFromFile.GetSymptoms();
+
+        // listOfSymptom.forEach((symptom) -> System.out.println(symptom));
+
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter(listOfSymptoms);
+
+        Map<String, Integer> symptomCounter = analyticsCounter.symptomCounter();
+
+        // symptomCounter.forEach((symptom, count) -> System.out.println(symptom + "=" + count));
+
+
+    }
+
+}
