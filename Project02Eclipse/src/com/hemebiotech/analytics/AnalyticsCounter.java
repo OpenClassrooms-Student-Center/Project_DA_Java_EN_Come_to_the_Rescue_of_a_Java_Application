0982be92ea1131376b1a4255f6233254860f5e50
Gareth NORMAN <norman.gareth@protonmail.com>
2022-05-04 13:21:43 +0800

    Creation class ReadAndStore, avec lecture du fichier, map et incrementation. Appel de la class et methode associe dans AnalyticsCounter.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,40 +1,11 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+
 
 public class AnalyticsCounter {
 	
-	public static void main(String args[]) throws Exception {
-		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
-		String line = reader.readLine();
-
-		int headAcheCount = 0;
-		int rashCount = 0;
-		int dialatedPupilsCount = 0;
-		
-		while (line != null) {	
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headAcheCount++;
-				System.out.println("number of headaches: " + headAcheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("dialated pupils")) {
-				dialatedPupilsCount++;
-			}
-
-			line = reader.readLine();
-		}
-		reader.close();
-		
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headAcheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + dialatedPupilsCount + "\n");
-		writer.close();
+	public static void main(String args[]) {
+		ReadAndStore listSymptomsMap = new ReadAndStore() ;
+		listSymptomsMap.symptomsReaderAndStorer();
 	}
 }
