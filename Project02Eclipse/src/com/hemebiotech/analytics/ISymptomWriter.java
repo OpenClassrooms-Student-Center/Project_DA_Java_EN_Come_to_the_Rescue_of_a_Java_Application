407c200ff7b493866069a51af36c169d437ed41a
Regis Eboa <regiseboa@gmail.com>
2022-05-05 13:00:47 +0200

    Ajout de commentaire de doc à l'interface ISymptom.java

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -1,5 +1,10 @@
 package com.hemebiotech.analytics;
 
+/** 
+ * Anything that will write symptom data on a source
+ * The important part is, the operation get symptom data from a LinkedHashMap,
+ * in sort order
+ */
 public interface ISymptomWriter {
     
     void writeSymptoms ();
