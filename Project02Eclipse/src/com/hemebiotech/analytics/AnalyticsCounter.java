86bbd67d0be0391ad79aef93f77825079c753ddb
Musei Senpai <musei.senpai@gmail.com>
2022-07-15 16:37:59 +0200

    Fixed Alex's code.
    - Made it cleaner (less comments, but more effective ones)
    - Removed useless and unused code lines and variables
    - Made the class' attribute 'private', to make it nicer on java purists eyes (lul :v)
    - Corrected the errors like "rush" instead of "rash" in the IF statement and used the correct variables
    to keep tracks of the GOOD symptoms
    => the program now returns valid results in the output file !

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,35 +5,26 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+	// initializing specific counters for three symptoms to 0
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
+
+	// read data from file line by line and get analytics
 	public static void main(String args[]) throws Exception {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
 		while (line != null) {
-			i++;	// increment i
 			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
+			if (line.equals("headache")) headacheCount++;
+			else if (line.equals("rash")) rashCount++;
+			else if (line.contains("pupils")) pupilCount++;
+			line = reader.readLine();
 		}
-		
-		// next generate output
+		reader.close();
+
+		// generate output
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
