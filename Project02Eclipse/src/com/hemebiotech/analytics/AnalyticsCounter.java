90fa15fd51e568ad33429b5068ca3e464c8ed7a9
StevenLignereux <slignere@gmail.com>
2022-08-10 09:31:46 +0200

    Third version with map

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,73 +3,88 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	/* private static int rashCount = 0;
-	private static int pupilCount = 0;*/
+/*    private static int rashCount = 0;
+    private static int pupilCount = 0;*/
 
     public static void main(String[] args) throws Exception {
-		/* BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+/*        BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+        String line = reader.readLine();
 
-		int i = 0;
-		int headCount = 0;
+        int i = 0;
+        int headCount = 0;
 
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
+        while (line != null) {
+            i++;
+            System.out.println("symptom from file: " + line);
 
-			if (line.equals("headache")) {
-				headCount++;
+            if (line.equals("headache")) {
+                headCount++;
 //				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+            } else if (line.equals("rush")) {
+                rashCount++;
+            } else if (line.contains("pupils")) {
+                pupilCount++;
+            }
 
-			line = reader.readLine();	// get another symptom
-		}
+            line = reader.readLine();    // get another symptom
+        }
 
 
-		FileWriter writer = new FileWriter ("result.out");
+        FileWriter writer = new FileWriter("result.out");
 
 //		int headacheCount = 0;
 
-		writer.write("headache: " + headCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();*/
+        writer.write("headache: " + headCount + "\n");
+        writer.write("rash: " + rashCount + "\n");
+        writer.write("dialated pupils: " + pupilCount + "\n");
+        writer.close();*/
+
+//        Utiliser les map pour pouvoir extraire les symptomes du fichier
 
         File file = new File("symptoms.txt");
 
-        String[] words;
-
+        Map<String, Integer> mapSymptomWithNumberOccurence = new TreeMap<>();
         FileReader fileReader = new FileReader(file);
 
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         String str;
-
-        String search = "fever";
-        int wordCount = 0;
+        
 
         while ((str = bufferedReader.readLine()) != null) {
-            words = str.split(" ");
-
-            for (String word : words) {
-                if (word.equals(search)) {
-                    wordCount++;
-                }
+            
+            if (mapSymptomWithNumberOccurence.containsKey(str)){
+                // on incrémente la valeur existante
+                int countOccurence = mapSymptomWithNumberOccurence.get(str);
+                countOccurence++;
+                mapSymptomWithNumberOccurence.replace(str, countOccurence);
+            } else {
+                mapSymptomWithNumberOccurence.put(str, 1);
             }
+
+            
+            // affficher le symptom
+//            words = str.split(" ");
+//
+//            for (String word : words) {
+//                if (word.equals(search)) {
+//                    wordCount++;
+//                }
+//            }
         }
 
-        if (wordCount != 0) {
-            System.out.println("Symptom " + search + ": " + wordCount);
-        } else {
-            System.out.println("Le mot ne se trouve pas dans le fichier!");
+        for (String symptom : mapSymptomWithNumberOccurence.keySet()) {
+            System.out.println(symptom + ":" + mapSymptomWithNumberOccurence.get(symptom));
         }
+
+//        if (wordCount != 0) {
+//            System.out.println("Symptom " + search + ": " + wordCount);
+//        } else {
+//            System.out.println("Le mot ne se trouve pas dans le fichier!");
+//        }
         fileReader.close();
 
     }
