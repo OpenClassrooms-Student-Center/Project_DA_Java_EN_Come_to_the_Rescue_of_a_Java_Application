0c072875f7111015905d6529c7be54ebfa297893
fredbuono <frederic.buono.devweb@gmail.com>
2022-08-21 15:43:29 +0200

    create map of symptoms and count

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,41 +3,44 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
 	
-	public static void main(String args[]) throws Exception {
+	public static void main(String[] args) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		BufferedReader reader = new BufferedReader(new FileReader("/Users/fredbuono/Documents/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt"));
 		String line = reader.readLine();
+		List<String> symptoms = new ArrayList<>();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+
+		// read all symptoms and create list
 		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
+			symptoms.add(line);
+			line = reader.readLine();
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		Map<String, Long> symptomsCount = symptoms.stream()
+				.collect(Collectors.groupingByConcurrent(s -> s, Collectors.counting()))
+				// sort by symptoms alphabetic
+				.entrySet().stream()
+				.sorted(Map.Entry.comparingByKey())
+				.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+						(oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+		// create output file
+		FileWriter writer = new FileWriter("result.out");
+
+		// write symptom and count
+		symptomsCount.forEach((s, aLong) -> {
+			try {
+				writer.write(s + ": " + aLong + "\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+
 		writer.close();
 	}
 }
