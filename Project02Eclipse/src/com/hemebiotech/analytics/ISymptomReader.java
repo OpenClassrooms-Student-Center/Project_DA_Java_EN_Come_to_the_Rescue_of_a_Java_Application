ce741356449efb4192c777c7cf015f85fe6d11ef
Alexandre Cabanis <dev.alex.cabanis@gmail.com>
2022-08-25 16:21:34 +0200

    Final version of my HemeBiotech program with Main Class,and some interfaces

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,20 +1,18 @@
 package com.hemebiotech.analytics;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
  * Anything that will read symptom data from a source
- * The important part is, the return value from the operation, which is a list of strings,
- * that may contain many duplications
- * 
- * The implementation does not need to order the list
- * 
+ * Return value from the operation, which is a list of strings, that may contain many duplications
  */
+
 public interface ISymptomReader {
 	/**
 	 * If no data is available, return an empty List
 	 * 
 	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
 	 */
-	List<String> GetSymptoms ();
+	List<String> GetSymptoms (String filename) throws IOException;
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,47 +1,46 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
  * Simple brute force implementation
- *
+ * ISymptomReader interface implementation 
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
-
-	private String filepath;
-	
-	/**
-	 * 
-	 * @param filepath a full or partial path to file with symptom strings in it, one per line
-	 */
-	public ReadSymptomDataFromFile (String filepath) {
-		this.filepath = filepath;
-	}
-	
-	@Override
-	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
 		
-		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
+
+		/* 
+		 * Get an access to the data file "Symptom.txt" and return result in a list of symptom
+		 */
+	
+		public List<String> GetSymptoms(String filename) throws IOException {
+			
+			System.out.println("Lecture du fichier : " + filename);
+			
+			BufferedReader symptom = new BufferedReader (new FileReader(filename));
+			
+			List<String> symptoms = new ArrayList<String>();
+			
+		
+			
+			String line = symptom.readLine();
+			
+			while (line !=null) {
 				
-				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
-				}
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+	
+				symptoms.add(line);
+				
+				line = symptom.readLine();
 			}
-		}
-		
-		return result;
-	}
+			
+			return symptoms;
+		} } 
 
-}
+
+
