7d382615d66cbe6024634ef6ce30a3d63eacc236
Moutaz <moutazabdelhadey@gmail.com>
2022-10-20 19:50:33 +0200

    Add exception + read method

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,6 +3,8 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Collections;
+import java.util.List;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
@@ -11,13 +13,19 @@ public class AnalyticsCounter {
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+		try {
+		// Read from data source
+		List <String> listSymptoms = read("symptoms.txt");
+		System.out.println(listSymptoms);
+		} catch (Exception e) {
+			System.out.println("Error occured : " + e);
+		}
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		int i = 0;
+		int headCount = 0;
+		String line;
 		while (line != null) {
-			i++;	// increment i
+			i++;
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
 				headCount++;
@@ -30,14 +38,21 @@ public class AnalyticsCounter {
 				pupilCount++;
 			}
 
-			line = reader.readLine();	// get another symptom
 		}
+	}
 		
+	// Return a list with data symptoms
+	static List<String> read(String nomFichier){
+		ISymptomReader symptoms = new ReadSymptomDataFromFile(nomFichier);
+		List<String> listSymptoms = symptoms.getSymptoms();
+		Collections.sort(listSymptoms);
+		return listSymptoms;
+	}
 		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
+		FileWriter writer = new FileWriter ("results.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
-	}
 }
+
