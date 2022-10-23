adebc08605c5f9d6266b83521d075e3c1036618a
Moutaz <moutazabdelhadey@gmail.com>
2022-10-23 10:20:42 +0200

    Writer correction

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/IWriteSymptom.java
@@ -0,0 +1,16 @@
+package com.hemebiotech.analytics;
+
+import java.util.Map;
+
+/**
+ * Anything that will write the count of occurrences from selected symptom data from an external source
+ * The important part is, the return value from the operation, which is a new file,
+ * containing the symptom selected with the count of its occurrence.
+ * 
+ * The implementation need to order the list by alphabetical
+ *
+ */
+public interface IWriteSymptom {
+	
+	void writeSymptom(Map<String, Long> mapSymptoms, String nomFichier);
+}
