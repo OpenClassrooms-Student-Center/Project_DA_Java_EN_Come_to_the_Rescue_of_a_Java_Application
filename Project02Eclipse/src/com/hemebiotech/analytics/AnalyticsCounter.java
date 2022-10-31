49d8b61883ebe90072229a393020c6ede4416886
Raphael Houdry <raphael-59200@hotmail.fr>
2022-10-31 12:36:12 +0100

    writingFile method now working

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -7,58 +7,31 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
+	
 	static File documentToParse = new File("symptoms.txt");
+	
 	static List<String> listOfSymptoms = new ArrayList<String>();
-	static HashSet<String> uniqueOccurence = new HashSet<String>();
 	static HashMap<String, Integer> symptomeFrequencyMap = new HashMap<String, Integer>();
 	
+	static FileWriter resultsDocument;
+	
 	public static void main(String args[]) throws Exception {
-
 		
-		/* 
-		int i = 0;
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-			
-			line = reader.readLine();	// get another symptom
-		}
+		resultsDocument = new FileWriter("result.out");
 		
-		reader.close(); 
-		*/ 
-		
-		parsingFile(documentToParse, listOfSymptoms, uniqueOccurence);
+		parsingFile(documentToParse, listOfSymptoms);
 		countingSymptoms(symptomeFrequencyMap, listOfSymptoms);
+		writingFile(resultsDocument, symptomeFrequencyMap);
 		
 		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
 	}
 
 	
 	// First parse the document and get a List of symptoms and a HashSet to count unique occurrences
-	public static void parsingFile(File documentToParse, List<String> listOfSymptoms, HashSet<String> uniqueOccurrence) {
+	public static void parsingFile(File documentToParse, List<String> listOfSymptoms) throws IOException{
 		if(documentToParse != null) {
 			try {
 				
@@ -67,7 +40,6 @@ public class AnalyticsCounter {
 				
 				while (symptom != null) {
 					listOfSymptoms.add(symptom);
-					uniqueOccurrence.add(symptom);
 					symptom = parsedFile.readLine();
 				}
 				parsedFile.close();
@@ -93,6 +65,19 @@ public class AnalyticsCounter {
 			}
 		}
 		System.out.println("FrequencyMap " + symptomeFrequencyMap);
+	}
+	
+	// This method will write the 
+	public static void writingFile(FileWriter resultsDocument, HashMap<String, Integer> symptomeFrequencyMap) throws IOException{
+		for(String symptom : symptomeFrequencyMap.keySet()) {
+			try {
+				resultsDocument.write(symptom + ": "  + symptomeFrequencyMap.get(symptom) +  "\n");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		resultsDocument.close();
 	}
 	
 }
