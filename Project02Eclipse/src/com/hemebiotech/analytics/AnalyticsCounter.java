0fb4ba24aa16ae005b62384cd0d8df8bfb5490a8
bruno.mazel <bruno.mazel@adelya.com>
2022-12-22 11:08:53 +0100

    FEAT : read/sort symptom
    lecture de n'importe quel symptome dans le fichier symptoms.txt / tri par ordre alphabétique des symptomes / affichage des symtpomes dans un fichier result.txt

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,6 +3,10 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
@@ -14,18 +18,41 @@ public class AnalyticsCounter {
 		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse/symptoms.txt"));
 		String line = reader.readLine();
 
-
+		Map<String, Integer> dico = new HashMap<String,Integer>();
+		List<String> listSymptom = new ArrayList<String>();
+		
 		while (line != null) {
 			
 			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("dialated pupils")) {
-				pupilCount++;
+			//ajout des symptoms
+			if (dico.containsKey(line)) {
+				dico.put(line, dico.get(line)+1);
+			}else {
+				listSymptom.add(line);
+				dico.put(line, 1);
+				// sort des symtpoms
+				if(listSymptom.size()==1) {
+					
+				}else if(listSymptom.size()==2) {
+					if(listSymptom.get(1).compareTo(listSymptom.get(0))<0) {
+						String tmp = listSymptom.get(0);
+						listSymptom.set(0, listSymptom.get(1));
+						listSymptom.set(1, tmp);
+					}
+				}else {
+					int indice = listSymptom.size()-1;
+					boolean flag = false;
+					while (indice>0 && !flag) {
+						if (listSymptom.get(indice-1).compareTo(listSymptom.get(indice))>0) {
+							String tmp = listSymptom.get(indice-1);
+							listSymptom.set(indice-1, listSymptom.get(indice));
+							listSymptom.set(indice, tmp);
+						}else {
+							flag = true;
+						}
+						indice--;
+					}
+				}
 			}
 
 			line = reader.readLine();	// get another symptom
