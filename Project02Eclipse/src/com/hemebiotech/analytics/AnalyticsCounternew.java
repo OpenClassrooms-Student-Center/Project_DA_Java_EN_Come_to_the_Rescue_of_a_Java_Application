b1795dc35d0601143fdf0bb355f204035a504ea1
ilhamami <ilhamami@ilhamami-VirtualBox>
2023-01-02 14:56:59 +0100

    Javadoc sur les 3 méthodes

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounternew.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounternew.java
@@ -21,15 +21,28 @@ public class AnalyticsCounternew implements IAnalyticsCounternew {
 
 	}
 
+	/**
+	 * Méthode pour récupèrer la liste des symptomes.
+	 * <p>
+	 * Cette méthode sert à lire le fichier symptoms.txt ligne par ligne et les
+	 * stocker dans la liste result . le Try catch utilise dans cette méthode permet
+	 * de gérer les exceptions.
+	 * <p>
+	 * 
+	 * @param fileName le nom de fichier symptoms.txt.
+	 * @param line     ligne.
+	 * @return la liste des symptoms stocker dans la liste result.
+	 */
 	@Override
-	// Definir la méthode qui récupère la liste des symptoms
+	// Définir la méthode qui récupère la liste des symptoms
 	public List<String> getSymptoms(String fileName) {
 		ArrayList<String> result = new ArrayList<String>();
 		if (fileName != null) {
 			try {
-				// Créer l'object fileReader
+				// une fonction à essayer pouvant générer une erreur
+				// Créer l'object fileReader pour lire le fichier
 				FileReader fileReader = new FileReader(fileName);
-				// Créer l'object reader
+				// Créer l'object reader qui utilise le fileReader
 				BufferedReader reader = new BufferedReader(fileReader);
 				// Lire le contenu du fichier
 				String line = reader.readLine();
@@ -49,6 +62,25 @@ public class AnalyticsCounternew implements IAnalyticsCounternew {
 
 	}
 
+	/**
+	 * Méthode pour calculer l'occurence de chaque symptoms et les trier par ordre
+	 * alphabétique.
+	 * <p>
+	 * Cette méthode sert à calculer l'occurence de chaque symptome. Cette occurence
+	 * est calculé en utilisant l'interface Map, la clé c'est le nom de symptome et
+	 * la valeur c'est son occurence. la classe TreeMap permet de trier les symptoms
+	 * dans l'ordre alphabétique.
+	 * <p>
+	 * 
+	 * @param listSymptoms la liste des symptomes.
+	 * @param symptom      utilise dans la boucle forEach pour parcourir la liste
+	 *                     listSymptoms.
+	 * @param oldOcurence  c'est la valeur de mapResult quand il prend la valeur de
+	 *                     symptom.
+	 * @param newOcurence  c'est la valeur de oldOcurence incrémente.
+	 * @return mapResult qui prend en clé le nom de symptome et en valeur son
+	 *         occurence trier par ordre alphabétique.
+	 */
 	@Override
 	// Definir la méthode qui regroupe le symptom par occurence
 	public Map<String, Integer> getSymptomsMapOcurence(List<String> listSymptoms) {
@@ -72,12 +104,25 @@ public class AnalyticsCounternew implements IAnalyticsCounternew {
 		return mapResult;
 	}
 
+	/**
+	 * Méthode pour écrire le resultat dans le fichier result.out.
+	 * <p>
+	 * Cette méthode sert à écrire dans le fichier result.out. le Try catch utilise
+	 * dans cette méthode permet de gérer les exceptions. FileWriter pour écrire
+	 * dans le fichier Utiliser la boucle for Each pour parcourir le Map
+	 * symptomsOcurenceMap.
+	 * <p>
+	 * 
+	 * @param fileNameOut le nom de fichier result.out
+	 * @param entry       utilise dans la boucle forEach pour parcourir
+	 *                    symptomsOcurenceMap.
+	 * @return fichier result.out.
+	 */
 	@Override
 	// definir la méthode qui écrire dans le fichier result.out
 	public void writerSymptoms(Map<String, Integer> symptomsOcurenceMap, String fileNameOut) throws IOException {
 
 		try {
-
 			File fichier = new File(fileNameOut);
 
 			FileWriter writer = new FileWriter(fichier);
