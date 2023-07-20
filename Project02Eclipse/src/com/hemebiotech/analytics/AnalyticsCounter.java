c364af3e55047b9d6ad45b9a5033ef70561c5c85
metaxiii <lehchibi.gael@pm.me>
2023-07-20 17:17:45 +0200

    feat: end of step 4

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,29 +3,35 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-  private static final int headacheCount = 0;
-  private static int rashCount = 0;
-  private static int pupilCount = 0;
+  private final ISymptomReader reader;
+  private final ISymptomWriter writer;
 
-  public static void main(String[] args) throws Exception {
-    BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-    String line = reader.readLine();
-    int i = 0;
-    int headCount = 0;
-    while (line != null) {
-      i++;
-      System.out.println("symptom from file: " + line);
-      if (line.equals("headache")) {
-        headCount++;
-        System.out.println("number of headaches: " + headCount);
-      } else if (line.equals("rush")) {
-        rashCount++;
-      } else if (line.contains("pupils")) {
-        pupilCount++;
-      }
-      line = reader.readLine();
-    }
+  public AnalyticsCounter(final ISymptomReader reader, final ISymptomWriter writer) {
+    this.reader = reader;
+    this.writer = writer;
+  }
+
+  public List<String> getSymptoms() {
+    return reader.retrieveSymptoms();
+  }
+
+  public Map<String, Integer> countSymptoms(final List<String> symptoms) {
+    final Map<String, Integer> map = new HashMap<>();
+    symptoms.forEach(s -> map.put(s, symptoms.stream().filter(word -> word.equals(s)).toList().size()));
+    return map;
+  }
+
+  public Map<String, Integer> sortSymptoms(final Map<String, Integer> symptoms) {
+    return new TreeMap<>(symptoms);
+  }
+
+  public void writeSymptoms(Map<String, Integer> symptoms) {
+    writer.writeSymptoms(symptoms);
   }
 }
