775374f403853c74cd1adf707266275dfe985837
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2023-09-19 22:08:25 +0200

    writer implementation

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -0,0 +1,25 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileWriter;
+import java.util.Map;
+
+public class WriteSymptomDataToFile implements ISymptomWriter {
+
+	private String filepath;
+
+	public WriteSymptomDataToFile(String filepath) {
+		this.filepath = filepath;
+	}
+
+	@Override
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		try (FileWriter writer = new FileWriter(filepath)) {
+			for (Map.Entry<String, Integer> symptom : symptoms.entrySet()) {
+				writer.write(symptom.getKey() + ": " + symptom.getValue() + "\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
