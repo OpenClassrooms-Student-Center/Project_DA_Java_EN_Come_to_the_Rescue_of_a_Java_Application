26a70b062e287eec6b82cbb982e6e68a051ec135
Shikizzz <widemann.simon@outlook.fr>
2023-10-12 17:03:31 +0200

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,49 +1,49 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.List;
 
 public class AnalyticsCounter {
+	ISymptomReader reader;
+	ISymptomWriter writer;
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+		this.reader = reader;
+		this.writer = writer;
+	}
 
-	private static Map<String, Integer> symptoms = new HashMap<String, Integer>();
+	public List<String> getSymptoms() {
+		return reader.GetSymptoms();
+	}
 
-	public static void lookForKeyInMap(Map<String, Integer> map, String symptom) {
-		boolean isInMap=false; //to track if we found the symptom in the map
-		for (String key : map.keySet()) {
-			if(symptom.equals(key)){
-				map.put(key, map.get(key)+1);
-				isInMap=true;
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
+		Map<String, Integer> result = new HashMap<>();
+		for(int i=0; i<symptoms.size(); i++){
+			boolean isInMap=false; //to track if we found the symptom in the map
+			for (String key : result.keySet()) {
+				if(symptoms.get(i).equals(key)){
+					result.put(key, result.get(key)+1);
+					isInMap=true;
+				}
+			}
+			if(!isInMap){
+				result.put(symptoms.get(i), 1);
 			}
 		}
-		if(!isInMap){
-			map.put(symptom, 1);
-		}
+		return result;
 	}
 
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+		TreeMap<String, Integer> sorted = new TreeMap<>(symptoms);
+		return sorted;
+	}
+
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		writer.writeSymptoms(symptoms);
+	}
 
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		while (line != null) {
-			lookForKeyInMap(symptoms, line);
-			line = reader.readLine();	// get another symptom
-		}
-		for(Map.Entry<String, Integer> entry : symptoms.entrySet()){
-			System.out.println(entry.getKey() + "->" + entry.getValue());
-		}
-		// next generate output
-		FileWriter fileWriter = new FileWriter ("result.out");
-		BufferedWriter writer = new BufferedWriter(fileWriter);
-		for(Map.Entry<String, Integer> entry : symptoms.entrySet()){
-			writer.write(entry.getKey() + ": " + entry.getValue());
-			writer.newLine();
-		}
-		writer.close();
-	}
+ 	}
 }
