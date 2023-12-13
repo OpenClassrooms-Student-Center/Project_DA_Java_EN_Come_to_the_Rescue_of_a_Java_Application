c4b50a3b90a7791bb4e62e00e34c27c42f6ea7a9
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-12-13 21:48:08 +0100

    ajout des commentaires

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -8,20 +8,22 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
 
     private String fileName;
 
+    // prend en parametre le nom de fichier "fileName"
     public WriteSymptomDataToFile(String fileName) {
         this.fileName = fileName;
     }
 
     @Override
+    // prend en paramettre une map contenant les symptoms
     public void writeSymptoms(Map<String, Integer> symptoms) {
         try {
-            FileWriter writer = new FileWriter(fileName, true);
-
+            FileWriter writer = new FileWriter(fileName);
+            // boucle sur la map pour récupérer les infos et les ecrirent ensuite
             for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
                 writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
             }
-            writer.close();
-        } catch (IOException e) {
+            writer.close(); // fermeture du writer
+        } catch (IOException e) { // on gere les exceptions dans le catch
             e.printStackTrace();
         }
     }
