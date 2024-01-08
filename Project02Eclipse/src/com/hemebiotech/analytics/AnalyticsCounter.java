48e33df9f6e2d8e73d0391c594ac2afe011415b2
Mushow <antoine@mushow.uk>
2024-01-08 21:24:40 +0100

    Removed unused imports

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,9 +1,5 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-
 public class AnalyticsCounter {
 
 	public static void main(String[] args) {
