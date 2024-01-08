574b852f200d0e7da7c11047e91a44f76270df3b
Mushow <antoine@mushow.uk>
2024-01-08 22:40:57 +0100

    Count symptoms using the Stream API, and using TreeMap to get a sorted map.

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomCounter.java
@@ -0,0 +1,9 @@
+package com.hemebiotech.analytics;
+
+import java.util.Map;
+
+public interface ISymptomCounter {
+
+    Map<String, Long> countSymptoms();
+
+}
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomsCounter.java
@@ -0,0 +1,22 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class SymptomsCounter implements ISymptomCounter {
+
+    private List<String> symptomsList;
+
+    public SymptomsCounter(List<String> symptomsList) {
+        this.symptomsList = symptomsList;
+    }
+
+    @Override
+    public Map<String, Long> countSymptoms() {
+        return symptomsList.stream()
+                .collect(Collectors.groupingBy(Function.identity(), TreeMap::new, Collectors.counting()));
+    }
+}
