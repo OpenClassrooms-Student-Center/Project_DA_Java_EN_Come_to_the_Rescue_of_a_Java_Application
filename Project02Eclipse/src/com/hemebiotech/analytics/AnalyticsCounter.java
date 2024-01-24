86d1a8793e8b9d450053d09449abaac026a5a6b3
PabloMiranda <pablo.miranda@sfr.fr>
2024-01-24 18:10:08 +0100

    remove unnecessary comments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,24 +4,27 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
+//TO FIX - Globally change the program structure to make it OOP
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	// TO FIX - All variable to be reorganised in a more scalable solution applying OOP
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
+		//TO FIX - First get txt file input, a new class to structure
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		//TO FIX - Reader Symptom Class to be developed
+		int i = 0;
+		int headCount = 0;
 		while (line != null) {
-			i++;	// increment i
+			i++;
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
 				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				System.out.println("number of headaches: " + headCount); //TO FIX, no need to console output, only a file is required
 			}
 			else if (line.equals("rush")) {
 				rashCount++;
@@ -30,10 +33,10 @@ public class AnalyticsCounter {
 				pupilCount++;
 			}
 
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine();	// get another symptom from the text file
 		}
 		
-		// next generate output
+		// TO FIX - To build an interface for this method (WRITER)
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
