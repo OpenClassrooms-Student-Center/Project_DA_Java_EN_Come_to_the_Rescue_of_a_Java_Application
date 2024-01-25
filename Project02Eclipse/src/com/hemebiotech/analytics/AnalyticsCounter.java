30c6020beaa1e8ca3e9a22f36dfb140228399e27
PabloMiranda <pablo.miranda@sfr.fr>
2024-01-25 18:56:31 +0100

    remove unnecessary comments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,3 +1,12 @@
+/**
+
+ * Project Analytics
+ * Frequency count of undesirable effects (symptoms) of patients testing drugs of Heme Biotech registered in a text file,
+ * where one line represents one occurrence
+ * @version: 0.2 2024-01-25
+ * @authors: Pablo Miranda, Alex OpenClassRooms, Caroline OpenClassRooms
+ */
+
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
@@ -6,13 +15,13 @@ import java.io.FileWriter;
 
 //TO FIX - Globally change the program structure to make it OOP
 public class AnalyticsCounter {
-	// TO FIX - All variable to be reorganised in a more scalable solution applying OOP
+	// TO FIX - All variables to be reorganised in a more scalable solution applying OOP
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
 	
 	public static void main(String args[]) throws Exception {
-		//TO FIX - First get txt file input, a new class to structure
+		//TO FIX - First get txt file input, a class needed here
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
@@ -33,10 +42,10 @@ public class AnalyticsCounter {
 				pupilCount++;
 			}
 
-			line = reader.readLine();	// get another symptom from the text file
+			line = reader.readLine();	// get another symptom from the text file, a class to do so
 		}
 		
-		// TO FIX - To build an interface for this method (WRITER)
+		// TO FIX - To build an interface for this subroutine (WRITER)
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
